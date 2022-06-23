68a46cf32c7dcf9d097e7e6db3f8273155ede373
Manu Agrawal <manu.agrawal@firecell.io>
2022-06-23 22:55:25 +0530

    SYS IND Task and Port Changes

--- openair2/COMMON/ss_messages_types.h
+++ openair2/COMMON/ss_messages_types.h
@@ -71,6 +71,9 @@
 #define SS_RRC_PDU_REQ(mSGpTR)                (mSGpTR)->ittiMsg.ss_rrc_pdu_req
 #define SS_RRC_PDU_IND(mSGpTR)                (mSGpTR)->ittiMsg.ss_rrc_pdu_ind
 
+/** SYS IND */
+#define SS_SYSTEM_IND(mSGpTR)                 (mSGpTR)->ittiMsg.ss_system_ind
+
 /** PORTMAN */
 typedef struct ss_sys_port_msg_ind {
   struct SYSTEM_CTRL_REQ* req;
---
+++ openair3/SS/ss_eNB_sysind_task.h
@@ -0,0 +1,8 @@
+#ifndef SS_ENB_SYSIND_TASK_H_
+#define SS_ENB_SYSIND_TASK_H_
+
+void  ss_eNB_sysind_init(void);
+void *ss_eNB_sysind_process_itti_msg(void *);
+void *ss_eNB_sysind_task(void *arg);
+
+#endif /* SS_ENB_SYSIND_TASK_H_ */                                
