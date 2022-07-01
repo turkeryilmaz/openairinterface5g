4f19fc5ee8efff879fd74c44fe029df0412488cf
Vaibhav Shrivastava <vaibhav.shrivastava@firecell.io>
2022-07-01 07:35:26 +0530

    Sending SYSTEM_IND based on rachpreamble_enable flag

--- openair2/LAYER2/MAC/eNB_scheduler_RA.c
+++ openair2/LAYER2/MAC/eNB_scheduler_RA.c
@@ -1427,17 +1427,19 @@ initiate_ra_proc(module_id_t module_idP,
             module_idP, CC_id, frameP, ra[i].Msg2_frame,
             ra[i].Msg2_subframe, i, ra[i].rnti, ra[i].state);
 
-      // Populate and send the SS_SYSTEM_IND to System Simulator
-      MessageDef *m = itti_alloc_new_message(TASK_MAC_ENB, 0, SS_SYSTEM_IND);
-      SS_SYSTEM_IND(m).bitmask = false;
-      SS_SYSTEM_IND(m).sfn = frameP;
-      SS_SYSTEM_IND(m).sf = subframeP;
-      SS_SYSTEM_IND(m).ra_PreambleIndex = preamble_index;
-      SS_SYSTEM_IND(m).prtPower_Type = true;
-      SS_SYSTEM_IND(m).repetitionsPerPreambleAttempt = 0;
-      itti_send_msg_to_task(TASK_SS_SYSIND, module_idP, m);
-      LOG_A(MAC,"MAC Sending SS_SYSTEM_IND to System Simulator\n");
-
+      if(RC.ss.l1macind.rachpreamble_enable)
+      {
+        // Populate and send the SS_SYSTEM_IND to System Simulator
+        MessageDef *m = itti_alloc_new_message(TASK_MAC_ENB, 0, SS_SYSTEM_IND);
+        SS_SYSTEM_IND(m).bitmask = false;
+        SS_SYSTEM_IND(m).sfn = frameP;
+        SS_SYSTEM_IND(m).sf = subframeP;
+        SS_SYSTEM_IND(m).ra_PreambleIndex = preamble_index;
+        SS_SYSTEM_IND(m).prtPower_Type = true;
+        SS_SYSTEM_IND(m).repetitionsPerPreambleAttempt = 0;
+        itti_send_msg_to_task(TASK_SS_SYSIND, module_idP, m);
+        LOG_A(MAC,"MAC Sending SS_SYSTEM_IND to System Simulator\n");
+      }
       return;
     }
   }
