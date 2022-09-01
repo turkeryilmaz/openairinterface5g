64f41e27e06a92414cfa803374dfa0936f388e52
Vaibhav Shrivastava <vaibhav.shrivastava@firecell.io>
2022-09-01 16:21:26 +0530

    VNF MultiCell Changes rebased

--- openair2/PHY_INTERFACE/IF_Module.c
+++ openair2/PHY_INTERFACE/IF_Module.c
@@ -721,7 +721,7 @@ void UL_indication(UL_IND_t *UL_info, void *proc) {
   }
 
   if (NFAPI_MODE != NFAPI_MODE_PNF) {
-    if (ifi->CC_mask==0) {
+    if ((ifi->CC_mask==0)||(ifi->CC_mask==1)) {
       ifi->current_frame    = UL_info->frame;
       ifi->current_subframe = UL_info->subframe;
     } else {
@@ -750,7 +750,7 @@ void UL_indication(UL_IND_t *UL_info, void *proc) {
   handle_ulsch(UL_info);
 
   if (NFAPI_MODE != NFAPI_MODE_PNF) {
-    if (ifi->CC_mask == ((1<<MAX_NUM_CCs)-1)) {
+    if (ifi->CC_mask == ((1<<MAX_NUM_CCs)-3)) {
       eNB_dlsch_ulsch_scheduler(module_id,
                                 (UL_info->frame+((UL_info->subframe>(9-sf_ahead))?1:0)) % 1024,
                                 (UL_info->subframe+sf_ahead)%10);
