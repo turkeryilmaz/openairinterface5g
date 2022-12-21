3e5e7abaab512bf4798a9cf50556a2f02f2d5945
Robert Schmidt <robert.schmidt@openairinterface.org>
2022-12-21 13:32:39 +0100

    Integrate E2 Agent with OAI config system

--- openair2/GNB_APP/gnb_config.h
+++ openair2/GNB_APP/gnb_config.h
@@ -110,5 +110,10 @@ void nr_read_config_and_init(void);
 MessageDef *RCconfig_NR_CU_E1(bool separate_CUUP_process);
 ngran_node_t get_node_type(void);
 
+#ifdef OAI_E2_AGENT
+#include "openair2/E2AP/agent/e2_agent_api.h"
+e2_agent_args_t RCconfig_NR_E2agent(void);
+#endif
+
 #endif /* GNB_CONFIG_H_ */
 /** @} */
