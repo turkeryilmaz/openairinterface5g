38dcebf582ea1b5c9dfd8e0b6c3018c035c43d4d
Ejaz Ahmed <ejaz@episci.com>
2023-03-13 17:37:29 -0700

    initial code for rx virtual resource block demapping

--- openair1/PHY/NR_UE_TRANSPORT/nr_slsch_demodulation.c
+++ openair1/PHY/NR_UE_TRANSPORT/nr_slsch_demodulation.c
@@ -308,66 +308,78 @@ void nr_slsch_extract_rbs(int32_t **rxdataF,
                           int slot,
                           unsigned char symbol,
                           uint8_t is_dmrs_symbol,
-                          nfapi_nr_pusch_pdu_t *pusch_pdu,
+                          nfapi_nr_pssch_pdu_t *pssch_pdu,
                           NR_DL_FRAME_PARMS *frame_parms) {
 
-  unsigned short start_re, re, nb_re_pusch;
+  unsigned short start_re, re, nb_re_pssch;
   unsigned char aarx, aatx;
   uint32_t rxF_ext_index = 0;
-  uint32_t ul_ch0_ext_index = 0;
-  uint32_t ul_ch0_index = 0;
-  int16_t *rxF,*rxF_ext;
-  int *ul_ch0,*ul_ch0_ext;
-  int soffset = (slot&3)*frame_parms->symbols_per_slot*frame_parms->ofdm_symbol_size;
+  uint32_t sl_ch0_ext_index = 0;
+  uint32_t sl_ch0_index = 0;
+  int16_t *rxF, *rxF_ext;
+  int *sl_ch0, *sl_ch0_ext;
+  uint16_t nb_re_sci1 = 0;
+  uint16_t n = 0;
+  uint8_t k_prime = 0;
+  if (1 <= symbol && symbol <= 3) {
+    nb_re_sci1 = NR_NB_SC_PER_RB * NB_RB_SCI1;
+  }
+  int soffset = (slot & 3) * frame_parms->symbols_per_slot * frame_parms->ofdm_symbol_size;
 
 #ifdef DEBUG_RB_EXT
   printf("--------------------symbol = %d-----------------------\n", symbol);
-  printf("--------------------ch_ext_index = %d-----------------------\n", symbol*NR_NB_SC_PER_RB * pusch_pdu->rb_size);
+  printf("--------------------ch_ext_index = %d-----------------------\n", symbol * NR_NB_SC_PER_RB * pusch_pdu->rb_size);
 #endif
 
   uint8_t is_data_re;
-  start_re = (frame_parms->first_carrier_offset + (pusch_pdu->rb_start + pusch_pdu->bwp_start) * NR_NB_SC_PER_RB)%frame_parms->ofdm_symbol_size;
-  nb_re_pusch = NR_NB_SC_PER_RB * pusch_pdu->rb_size;
+  start_re = (frame_parms->first_carrier_offset + (pssch_pdu->rb_start + pssch_pdu->bwp_start) * NR_NB_SC_PER_RB) % frame_parms->ofdm_symbol_size;
+  if (1 <= symbol && symbol <= 3) {
+    start_re += nb_re_sci1;
+  }
+  nb_re_pssch = NR_NB_SC_PER_RB * pssch_pdu->rb_size;
 
 #ifdef __AVX2__
-  int nb_re_pusch2 = nb_re_pusch + (nb_re_pusch&7);
+  int nb_re_pssch2 = nb_re_pssch + (nb_re_pssch & 7);
 #else
-  int nb_re_pusch2 = nb_re_pusch;
+  int nb_re_pssch2 = nb_re_pssch;
 #endif
 
   for (aarx = 0; aarx < frame_parms->nb_antennas_rx; aarx++) {
 
-    rxF = (int16_t *)&rxdataF[aarx][soffset+(symbol * frame_parms->ofdm_symbol_size)];
-    rxF_ext = (int16_t *)&pusch_vars->rxdataF_ext[aarx][symbol * nb_re_pusch2]; // [hna] rxdataF_ext isn't contiguous in order to solve an alignment problem ib llr computation in case of mod_order = 4, 6
-    
+    rxF = (int16_t *)&rxdataF[aarx][soffset + (symbol * frame_parms->ofdm_symbol_size)];
+    rxF_ext = (int16_t *)&pssch_vars->rxdataF_ext[aarx][symbol * nb_re_pssch2]; // [hna] rxdataF_ext isn't contiguous in order to solve an alignment problem ib llr computation in case of mod_order = 4, 6
+
     if (is_dmrs_symbol == 0) {
-      if (start_re + nb_re_pusch <= frame_parms->ofdm_symbol_size) {
-        memcpy1((void*)rxF_ext, (void*)&rxF[start_re*2], nb_re_pusch*sizeof(int32_t));
+      uint16_t nb_re = 0;
+      nb_re = start_re + (nb_re_pssch - nb_re_sci1);
+      if ( nb_re <= frame_parms->ofdm_symbol_size) {
+          memcpy1((void*)rxF_ext, (void*)&rxF[start_re * 2], (nb_re_pssch - nb_re_sci1) * sizeof(int32_t));
       } else {
-        int neg_length = frame_parms->ofdm_symbol_size-start_re;
-        int pos_length = nb_re_pusch-neg_length;
-        memcpy1((void*)rxF_ext,(void*)&rxF[start_re*2],neg_length*sizeof(int32_t));
-        memcpy1((void*)&rxF_ext[2*neg_length],(void*)rxF,pos_length*sizeof(int32_t));
+        int neg_length = frame_parms->ofdm_symbol_size - start_re;
+        int pos_length = nb_re_pssch - neg_length;
+        memcpy1((void*)rxF_ext, (void*)&rxF[start_re * 2], neg_length * sizeof(int32_t));
+        memcpy1((void*)&rxF_ext[2 * neg_length], (void*)rxF, pos_length * sizeof(int32_t));
       }
 
-      for (aatx = 0; aatx < pusch_pdu->nrOfLayers; aatx++) {
-        ul_ch0 = &pusch_vars->ul_ch_estimates[aatx*frame_parms->nb_antennas_rx+aarx][pusch_vars->dmrs_symbol*frame_parms->ofdm_symbol_size]; // update channel estimates if new dmrs symbol are available
-        ul_ch0_ext = &pusch_vars->ul_ch_estimates_ext[aatx*frame_parms->nb_antennas_rx+aarx][symbol*nb_re_pusch2];
-        memcpy1((void*)ul_ch0_ext,(void*)ul_ch0,nb_re_pusch*sizeof(int32_t));
+      for (aatx = 0; aatx < pssch_pdu->nrOfLayers; aatx++) {
+        sl_ch0 = &pssch_vars->sl_ch_estimates[aatx * frame_parms->nb_antennas_rx + aarx][pssch_vars->dmrs_symbol * frame_parms->ofdm_symbol_size]; // update channel estimates if new dmrs symbol are available
+        sl_ch0_ext = &pssch_vars->sl_ch_estimates_ext[aatx * frame_parms->nb_antennas_rx + aarx][symbol * nb_re_pssch2];
+        memcpy1((void*)sl_ch0_ext, (void*)sl_ch0, nb_re_pssch * sizeof(int32_t));
       }
 
-    } else {
-
-      for (aatx = 0; aatx < pusch_pdu->nrOfLayers; aatx++) {
-        ul_ch0 = &pusch_vars->ul_ch_estimates[aatx*frame_parms->nb_antennas_rx+aarx][pusch_vars->dmrs_symbol*frame_parms->ofdm_symbol_size]; // update channel estimates if new dmrs symbol are available
-        ul_ch0_ext = &pusch_vars->ul_ch_estimates_ext[aatx*frame_parms->nb_antennas_rx+aarx][symbol*nb_re_pusch2];
+    } else { // DMRS case
+      uint16_t num_of_sci2_re = 0;
+      for (aatx = 0; aatx < pssch_pdu->nrOfLayers; aatx++) {
+        sl_ch0 = &pssch_vars->sl_ch_estimates[aatx * frame_parms->nb_antennas_rx + aarx][pssch_vars->dmrs_symbol * frame_parms->ofdm_symbol_size]; // update channel estimates if new dmrs symbol are available
+        sl_ch0_ext = &pssch_vars->sl_ch_estimates_ext[aatx * frame_parms->nb_antennas_rx + aarx][symbol * nb_re_pssch2];
 
         rxF_ext_index = 0;
-        ul_ch0_ext_index = 0;
-        ul_ch0_index = 0;
-        for (re = 0; re < nb_re_pusch; re++) {
+        sl_ch0_ext_index = 0;
+        sl_ch0_index = 0;
+        for (re = 0; re < nb_re_pssch; re++) {
+          uint8_t is_dmrs = 0;
           uint16_t k = start_re + re;
-          is_data_re = allowed_xlsch_re_in_dmrs_symbol(k, start_re, frame_parms->ofdm_symbol_size, pusch_pdu->num_dmrs_cdm_grps_no_data, pusch_pdu->dmrs_config_type);
+          is_data_re = allowed_xlsch_re_in_dmrs_symbol(k, start_re, frame_parms->ofdm_symbol_size, pssch_pdu->num_dmrs_cdm_grps_no_data, pssch_pdu->dmrs_config_type);
           if (++k >= frame_parms->ofdm_symbol_size) {
             k -= frame_parms->ofdm_symbol_size;
           }
@@ -379,21 +391,21 @@ void nr_slsch_extract_rbs(int32_t **rxdataF,
           // save only data and respective channel estimates
           if (is_data_re == 1) {
             if (aatx == 0) {
-              rxF_ext[rxF_ext_index]     = (rxF[ ((start_re + re)*2)      % (frame_parms->ofdm_symbol_size*2)]);
-              rxF_ext[rxF_ext_index + 1] = (rxF[(((start_re + re)*2) + 1) % (frame_parms->ofdm_symbol_size*2)]);
-              rxF_ext_index +=2;
+              rxF_ext[rxF_ext_index]     = (rxF[ ((start_re + re) * 2)      % (frame_parms->ofdm_symbol_size * 2)]);
+              rxF_ext[rxF_ext_index + 1] = (rxF[(((start_re + re) * 2) + 1) % (frame_parms->ofdm_symbol_size * 2)]);
+              rxF_ext_index += 2;
             }
-          
-            ul_ch0_ext[ul_ch0_ext_index] = ul_ch0[ul_ch0_index];
-            ul_ch0_ext_index++;
+
+            sl_ch0_ext[sl_ch0_ext_index] = sl_ch0[sl_ch0_index];
+            sl_ch0_ext_index++;
 
             #ifdef DEBUG_RB_EXT
             printf("dmrs symb %d: rxF_ext[%d] = (%d,%d), ul_ch0_ext[%d] = (%d,%d)\n",
-                 is_dmrs_symbol,rxF_ext_index>>1, rxF_ext[rxF_ext_index],rxF_ext[rxF_ext_index+1],
-                 ul_ch0_ext_index,  ((int16_t*)&ul_ch0_ext[ul_ch0_ext_index])[0],  ((int16_t*)&ul_ch0_ext[ul_ch0_ext_index])[1]);
-            #endif          
-          } 
-          ul_ch0_index++;
+                 is_dmrs_symbol, rxF_ext_index >> 1, rxF_ext[rxF_ext_index], rxF_ext[rxF_ext_index + 1],
+                 sl_ch0_ext_index,  ((int16_t*)&sl_ch0_ext[sl_ch0_ext_index])[0],  ((int16_t*)&sl_ch0_ext[sl_ch0_ext_index])[1]);
+            #endif
+          }
+          sl_ch0_index++;
         }
       }
     }
@@ -2016,7 +2028,8 @@ void nr_ue_rx_pssch(PHY_VARS_NR_UE *nrUE,
     //----------------------------------------------------------
     if (nb_re_pusch > 0) {
       start_meas(&nrUE->slsch_rbs_extraction_stats);
-      nr_slsch_extract_rbs(nrUE->common_vars.rxdataF,
+      int32_t **rxdataF = nrUE->common_vars.common_vars_rx_data_per_thread[proc->thread_id].rxdataF;
+      nr_slsch_extract_rbs(rxdataF,
                            nrUE->pssch_vars[dlsch_id],
                            slot,
                            symbol,
