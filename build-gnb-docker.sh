d67c03a264819789ecc9c1ce7edb8b8a7d7c0146
Michael Dürre <michael.duerre@highstreet-technologies.com>
2023-03-14 11:13:50 +0000

    fix build script

--- build-gnb-docker.sh
+++ build-gnb-docker.sh
@@ -35,17 +35,21 @@ if $ALL_STAGES; then
         exit 1
     fi
 fi
+
+docker build $NO_CACHE --target ran-build --build-arg BASETAG=$OSBASE-latest --tag ran-build:$OSBASE-latest --file docker/Dockerfile.build.$OSBASE .
+if [ ! $? -eq 0 ]; then
+    exit 1
+fi
 if $ONLY_DEV_BUILD; then
     docker build $NO_CACHE --target ran-dev --build-arg BASETAG=$OSBASE-latest --tag ran-dev:$OSBASE-latest --file docker/Dockerfile.dev.$OSBASE .
     if [ ! $? -eq 0 ]; then
         exit 1
     fi
-    exit 0
+    if ! $ALL_STAGES; then
+        exit 0
+    fi
 fi
-docker build $NO_CACHE --target ran-build --build-arg BASETAG=$OSBASE-latest --tag ran-build:$OSBASE-latest --file docker/Dockerfile.build.$OSBASE .
-if [ ! $? -eq 0 ]; then
-    exit 1
-fi
+
 docker build $NO_CACHE --target oai-gnb --build-arg BASETAG=$OSBASE-latest --tag oai-gnb:$OSBASE-latest --tag oai-gnb:$TIMESTAMP_TAG --file docker/Dockerfile.gNB.ubuntu22 .
 if [ ! $? -eq 0 ]; then
         exit 1
