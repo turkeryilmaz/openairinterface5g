77b529c353bd052294ed172b2fe9d4faea8a3400
Anurag Asokan <anurag.asokan@firecell.io>
2023-03-16 12:11:51 +0000

    Final working changes

--- openair3/SS/ss_eNB_drb_task.h
+++ openair3/SS/ss_eNB_drb_task.h
@@ -4,6 +4,7 @@
 void  ss_eNB_drb_init(void);
 void *ss_eNB_drb_process_itti_msg(void *);
 void *ss_eNB_drb_task(void *arg);
+void *ss_eNB_drb_acp_task(void *arg);
 
 #endif /* SS_ENB_DRB_TASK_H_ */
 
