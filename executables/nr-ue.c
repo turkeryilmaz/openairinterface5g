50fbd7fe2ee43c51c70ede3e1d9e7f6e8d31fc1f
Stone WU <swu@sequans.com>
2023-03-21 22:43:39 +0800

    bug #126520: fix nrUE uplink NR_SLOT_INDICATION with wrong slot

--- executables/nr-ue.c
+++ executables/nr-ue.c
@@ -297,7 +297,6 @@ static void send_vt_slot_ack(nfapi_ue_slot_indication_vt_t *vt_ue_slot_ind, uint
         NR_UL_IND_t ul_info = {
                 .vt_ue_slot_ind = *vt_ue_slot_ind,
         };
-        check_and_process_slot_ind(vt_ue_slot_ind,  NFAPI_SFNSLOT2SFN(sfn_slot), NFAPI_SFNSLOT2SLOT(sfn_slot) );
         send_nsa_standalone_msg(&ul_info, vt_ue_slot_ind->header.message_id);
         ul_info.vt_ue_slot_ind.sfn = 0;
         ul_info.vt_ue_slot_ind.slot = 0;
