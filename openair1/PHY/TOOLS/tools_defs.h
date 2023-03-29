fc3c1c6f13c5db17ba9f90f9d8bae3a217ead0b8
Robert Schmidt <robert.schmidt@openairinterface.org>
2023-03-29 09:52:13 +0200

    Include SIMDE avx2 functions in tools_defs.h

--- openair1/PHY/TOOLS/tools_defs.h
+++ openair1/PHY/TOOLS/tools_defs.h
@@ -36,6 +36,7 @@
 #include "common/utils/assertions.h"
 #include <simde/simde-common.h>
 #include <simde/x86/sse.h>
+#include <simde/x86/avx2.h>
 
 #define simd_q15_t simde__m128i
 #define simdshort_q15_t simde__m64
