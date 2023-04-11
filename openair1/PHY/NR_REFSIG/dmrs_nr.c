dfki-in-icc/5G-PRACH-Smart-Jammer

intermediate commit

openair1/PHY/NR_REFSIG/dmrs_nr.c
@@ -347,8 +347,8 @@ void nr_chest_time_domain_avg(NR_DL_FRAME_PARMS *frame_parms,
                               uint16_t dmrs_bitmap,
                               uint16_t num_rbs)
 {
-  __m128i *ul_ch128_0;
-  __m128i *ul_ch128_1;
+  simde__m128i *ul_ch128_0;
+  simde__m128i *ul_ch128_1;
   int16_t *ul_ch16_0;
   int total_symbols = start_symbol + num_symbols;
   int num_dmrs_symb = get_dmrs_symbols_in_slot(dmrs_bitmap, total_symbols);
@@ -356,31 +356,31 @@ void nr_chest_time_domain_avg(NR_DL_FRAME_PARMS *frame_parms,
   AssertFatal(first_dmrs_symb > -1, "No DMRS symbol present in this slot\n");
   for (int aarx = 0; aarx < frame_parms->nb_antennas_rx; aarx++) {
     for (int symb = first_dmrs_symb+1; symb < total_symbols; symb++) {
-      ul_ch128_0 = (__m128i *)&ch_est[aarx][first_dmrs_symb*frame_parms->ofdm_symbol_size];
+      ul_ch128_0 = (simde__m128i *)&ch_est[aarx][first_dmrs_symb*frame_parms->ofdm_symbol_size];
       if ((dmrs_bitmap >> symb) & 0x01) {
-        ul_ch128_1 = (__m128i *)&ch_est[aarx][symb*frame_parms->ofdm_symbol_size];
+        ul_ch128_1 = (simde__m128i *)&ch_est[aarx][symb*frame_parms->ofdm_symbol_size];
         for (int rbIdx = 0; rbIdx < num_rbs; rbIdx++) {
-          ul_ch128_0[0] = _mm_adds_epi16(ul_ch128_0[0], ul_ch128_1[0]);
-          ul_ch128_0[1] = _mm_adds_epi16(ul_ch128_0[1], ul_ch128_1[1]);
-          ul_ch128_0[2] = _mm_adds_epi16(ul_ch128_0[2], ul_ch128_1[2]);
+          ul_ch128_0[0] = simde_mm_adds_epi16(ul_ch128_0[0], ul_ch128_1[0]);
+          ul_ch128_0[1] = simde_mm_adds_epi16(ul_ch128_0[1], ul_ch128_1[1]);
+          ul_ch128_0[2] = simde_mm_adds_epi16(ul_ch128_0[2], ul_ch128_1[2]);
           ul_ch128_0 += 3;
           ul_ch128_1 += 3;
         }
       }
     }
-    ul_ch128_0 = (__m128i *)&ch_est[aarx][first_dmrs_symb*frame_parms->ofdm_symbol_size];
+    ul_ch128_0 = (simde__m128i *)&ch_est[aarx][first_dmrs_symb*frame_parms->ofdm_symbol_size];
     if (num_dmrs_symb == 2) {
       for (int rbIdx = 0; rbIdx < num_rbs; rbIdx++) {
-        ul_ch128_0[0] = _mm_srai_epi16(ul_ch128_0[0], 1);
-        ul_ch128_0[1] = _mm_srai_epi16(ul_ch128_0[1], 1);
-        ul_ch128_0[2] = _mm_srai_epi16(ul_ch128_0[2], 1);
+        ul_ch128_0[0] = simde_mm_srai_epi16(ul_ch128_0[0], 1);
+        ul_ch128_0[1] = simde_mm_srai_epi16(ul_ch128_0[1], 1);
+        ul_ch128_0[2] = simde_mm_srai_epi16(ul_ch128_0[2], 1);
         ul_ch128_0 += 3;
       }
     } else if (num_dmrs_symb == 4) {
       for (int rbIdx = 0; rbIdx < num_rbs; rbIdx++) {
-        ul_ch128_0[0] = _mm_srai_epi16(ul_ch128_0[0], 2);
-        ul_ch128_0[1] = _mm_srai_epi16(ul_ch128_0[1], 2);
-        ul_ch128_0[2] = _mm_srai_epi16(ul_ch128_0[2], 2);
+        ul_ch128_0[0] = simde_mm_srai_epi16(ul_ch128_0[0], 2);
+        ul_ch128_0[1] = simde_mm_srai_epi16(ul_ch128_0[1], 2);
+        ul_ch128_0[2] = simde_mm_srai_epi16(ul_ch128_0[2], 2);
         ul_ch128_0 += 3;
       }
     } else if (num_dmrs_symb == 3) {
@@ -415,4 +415,3 @@ void nr_chest_time_domain_avg(NR_DL_FRAME_PARMS *frame_parms,
     } else AssertFatal((num_dmrs_symb < 5) && (num_dmrs_symb > 0), "Illegal number of DMRS symbols in the slot\n");
   }
 }
-

turkeryilmaz/openairinterface5g

openair1/PHY/NR_REFSIG/dmrs_nr.c
@@ -347,8 +347,8 @@ void nr_chest_time_domain_avg(NR_DL_FRAME_PARMS *frame_parms,
                               uint16_t dmrs_bitmap,
                               uint16_t num_rbs)
 {
-  __m128i *ul_ch128_0;
-  __m128i *ul_ch128_1;
+  simde__m128i *ul_ch128_0;
+  simde__m128i *ul_ch128_1;
   int16_t *ul_ch16_0;
   int total_symbols = start_symbol + num_symbols;
   int num_dmrs_symb = get_dmrs_symbols_in_slot(dmrs_bitmap, total_symbols);
@@ -356,31 +356,31 @@ void nr_chest_time_domain_avg(NR_DL_FRAME_PARMS *frame_parms,
   AssertFatal(first_dmrs_symb > -1, "No DMRS symbol present in this slot\n");
   for (int aarx = 0; aarx < frame_parms->nb_antennas_rx; aarx++) {
     for (int symb = first_dmrs_symb+1; symb < total_symbols; symb++) {
-      ul_ch128_0 = (__m128i *)&ch_estimates[aarx][first_dmrs_symb*frame_parms->ofdm_symbol_size];
+      ul_ch128_0 = (simde__m128i *)&ch_estimates[aarx][first_dmrs_symb*frame_parms->ofdm_symbol_size];
       if ((dmrs_bitmap >> symb) & 0x01) {
-        ul_ch128_1 = (__m128i *)&ch_estimates[aarx][symb*frame_parms->ofdm_symbol_size];
+        ul_ch128_1 = (simde__m128i *)&ch_estimates[aarx][symb*frame_parms->ofdm_symbol_size];
         for (int rbIdx = 0; rbIdx < num_rbs; rbIdx++) {
-          ul_ch128_0[0] = _mm_adds_epi16(ul_ch128_0[0], ul_ch128_1[0]);
-          ul_ch128_0[1] = _mm_adds_epi16(ul_ch128_0[1], ul_ch128_1[1]);
-          ul_ch128_0[2] = _mm_adds_epi16(ul_ch128_0[2], ul_ch128_1[2]);
+          ul_ch128_0[0] = simde_mm_adds_epi16(ul_ch128_0[0], ul_ch128_1[0]);
+          ul_ch128_0[1] = simde_mm_adds_epi16(ul_ch128_0[1], ul_ch128_1[1]);
+          ul_ch128_0[2] = simde_mm_adds_epi16(ul_ch128_0[2], ul_ch128_1[2]);
           ul_ch128_0 += 3;
           ul_ch128_1 += 3;
         }
       }
     }
-    ul_ch128_0 = (__m128i *)&ch_estimates[aarx][first_dmrs_symb*frame_parms->ofdm_symbol_size];
+    ul_ch128_0 = (simde__m128i *)&ch_estimates[aarx][first_dmrs_symb*frame_parms->ofdm_symbol_size];
     if (num_dmrs_symb == 2) {
       for (int rbIdx = 0; rbIdx < num_rbs; rbIdx++) {
-        ul_ch128_0[0] = _mm_srai_epi16(ul_ch128_0[0], 1);
-        ul_ch128_0[1] = _mm_srai_epi16(ul_ch128_0[1], 1);
-        ul_ch128_0[2] = _mm_srai_epi16(ul_ch128_0[2], 1);
+        ul_ch128_0[0] = simde_mm_srai_epi16(ul_ch128_0[0], 1);
+        ul_ch128_0[1] = simde_mm_srai_epi16(ul_ch128_0[1], 1);
+        ul_ch128_0[2] = simde_mm_srai_epi16(ul_ch128_0[2], 1);
         ul_ch128_0 += 3;
       }
     } else if (num_dmrs_symb == 4) {
       for (int rbIdx = 0; rbIdx < num_rbs; rbIdx++) {
-        ul_ch128_0[0] = _mm_srai_epi16(ul_ch128_0[0], 2);
-        ul_ch128_0[1] = _mm_srai_epi16(ul_ch128_0[1], 2);
-        ul_ch128_0[2] = _mm_srai_epi16(ul_ch128_0[2], 2);
+        ul_ch128_0[0] = simde_mm_srai_epi16(ul_ch128_0[0], 2);
+        ul_ch128_0[1] = simde_mm_srai_epi16(ul_ch128_0[1], 2);
+        ul_ch128_0[2] = simde_mm_srai_epi16(ul_ch128_0[2], 2);
         ul_ch128_0 += 3;
       }
     } else if (num_dmrs_symb == 3) {
@@ -415,4 +415,3 @@ void nr_chest_time_domain_avg(NR_DL_FRAME_PARMS *frame_parms,
     } else AssertFatal((num_dmrs_symb < 5) && (num_dmrs_symb > 0), "Illegal number of DMRS symbols in the slot\n");
   }
 }
-