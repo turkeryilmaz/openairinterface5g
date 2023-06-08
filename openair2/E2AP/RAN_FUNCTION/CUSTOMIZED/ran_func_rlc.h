3aee5a38692f1eb2dd63c986e8cb055264291d91
Chieh-Chun Chen <chenc80348@gmail.com>
2023-06-08 16:36:43 +0200

    Enable RAN function for RLC SM

--- openair2/E2AP/RAN_FUNCTION/CUSTOMIZED/ran_func_rlc.h
+++ openair2/E2AP/RAN_FUNCTION/CUSTOMIZED/ran_func_rlc.h
@@ -1,7 +1,15 @@
 #ifndef RAN_FUNC_SM_RLC_READ_WRITE_AGENT_H
 #define RAN_FUNC_SM_RLC_READ_WRITE_AGENT_H
 
-#include "../../flexric/src/agent/e2_agent_api.h"
+#include "openair2/E2AP/flexric/src/agent/e2_agent_api.h"
+#include "common/ran_context.h"
+#include "openair2/LAYER2/NR_MAC_gNB/mac_proto.h"
+#include "openair2/LAYER2/nr_rlc/nr_rlc_oai_api.h"
+#include "openair2/E2AP/flexric/src/util/time_now_us.h"
+
+uint32_t num_act_rb(NR_UEs_t* const UE_info);
+
+void active_avg_to_tx(NR_UEs_t* const UE_info);
 
 void read_rlc_sm(void*);
 
