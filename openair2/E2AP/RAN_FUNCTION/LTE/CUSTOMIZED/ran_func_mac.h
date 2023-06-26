5444a5bb215dffeb044b961c494f6f9a67b8a9cd
Chieh-Chun Chen <chenc80348@gmail.com>
2023-06-26 18:25:56 +0200

    Add RAN function for LTE MAC

--- openair2/E2AP/RAN_FUNCTION/LTE/CUSTOMIZED/ran_func_mac.h
+++ openair2/E2AP/RAN_FUNCTION/LTE/CUSTOMIZED/ran_func_mac.h
@@ -2,6 +2,9 @@
 #define SM_MAC_READ_WRITE_AGENT_H
 
 #include "openair2/E2AP/flexric/src/agent/e2_agent_api.h"
+#include "common/ran_context.h"
+#include "openair2/LAYER2/MAC/mac.h"
+#include "openair2/E2AP/flexric/src/util/time_now_us.h"
 
 void read_mac_sm(void*);
 
