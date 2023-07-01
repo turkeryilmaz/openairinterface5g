7858c7729fbd4909c612bf939bf4c62d93979cbc
David Kim <david.kim@episci.com>
2023-06-30 20:44:29 -0700

    Type conversion to resolve crash in simulator.

--- openair1/PHY/NR_UE_TRANSPORT/sss_nr.c
+++ openair1/PHY/NR_UE_TRANSPORT/sss_nr.c
@@ -121,7 +121,7 @@ void init_context_sss_nr(int amp)
 
 //#define DEBUG_SSS_NR
 //#define DEBUG_PLOT_SSS
-void insert_sss_nr(c16_t *sss_time,
+void insert_sss_nr(int16_t *sss_time,
                    NR_DL_FRAME_PARMS *frame_parms)
 {
   const unsigned int ofdm_symbol_size = frame_parms->ofdm_symbol_size;
@@ -163,7 +163,7 @@ void insert_sss_nr(c16_t *sss_time,
   }
 
   /* get sss in the frequency domain by applying an inverse FFT */
-  c16_t out[sizeof(int16_t) * ofdm_symbol_size] __attribute__((aligned(32)));
+  int16_t out[sizeof(int16_t) * ofdm_symbol_size] __attribute__((aligned(32)));
   memset(out, 0, sizeof(out));
   memset(sss_time, 0, sizeof(int16_t) * ofdm_symbol_size);
   idft(IDFT_2048, (int16_t *)&in, (int16_t *)&out, 1);
@@ -188,15 +188,15 @@ static int pss_ch_est_nr(PHY_VARS_NR_UE *ue,
                          c16_t pss_ext[NB_ANTENNAS_RX][LENGTH_PSS_NR],
                          c16_t sss_ext[NB_ANTENNAS_RX][LENGTH_SSS_NR])
 {
-  c16_t *pss = get_primary_synchro_nr2(ue->common_vars.nid2);
+  int16_t *pss = get_primary_synchro_nr2(ue->common_vars.nid2);
 
   for (int aarx = 0; aarx < ue->frame_parms.nb_antennas_rx; aarx++) {
     c16_t *sss_ext2 = sss_ext[aarx];
     c16_t *pss_ext2 = pss_ext[aarx];
     for (int i = 0; i < LENGTH_PSS_NR; i++) {
       // This is H*(PSS) = R* \cdot PSS
-      const int tmp_re = pss_ext2[i].r * pss[i].r;
-      const int tmp_im = -pss_ext2[i].i * pss[i].i;
+      const int tmp_re = pss_ext2[i].r * pss[i];
+      const int tmp_im = -pss_ext2[i].i * pss[i];
 
       const int32_t amp = tmp_re * tmp_re + tmp_im * tmp_im;
       const int shift = log2_approx(amp) / 2;
