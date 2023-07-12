32df7bcf80a1c9c68b8718aa0ba62466557d0709
Tsung-Yu Chan <tsung-yu.chan@eurecom.fr>
2023-07-12 15:08:52 +0200

    feat / add the toolchain files for cross-compiler

---
+++ cross-arm.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER   /usr/bin/aarch64-linux-gnu-gcc-9)
+set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++-9)
+
+set(CROSS_COMPILER 1)
+
