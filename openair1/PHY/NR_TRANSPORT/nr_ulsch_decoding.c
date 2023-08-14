8054dba74d1b59d9d7739c8294c7a986478e0c50
mir <mikel.irazabal@openairinterface.com>
2023-08-14 12:05:42 +0200

    Unused time_now_us

--- openair1/PHY/NR_TRANSPORT/nr_ulsch_decoding.c
+++ openair1/PHY/NR_TRANSPORT/nr_ulsch_decoding.c
@@ -55,32 +55,6 @@
 #include <time.h>
 #include <stdalign.h>
 
-
-static inline
-int64_t time_now_us(void)
-{
-  struct timespec tms;
-
-  /* The C11 way */
-  /* if (! timespec_get(&tms, TIME_UTC))  */
-
-  /* POSIX.1-2008 way */
-  if (clock_gettime(CLOCK_REALTIME,&tms)) {
-    return -1;
-  }
-  /* seconds, multiplied with 1 million */
-  int64_t micros = tms.tv_sec * 1000000;
-  /* Add full microseconds */
-  micros += tms.tv_nsec/1000;
-  /* round up if necessary */
-  if (tms.tv_nsec % 1000 >= 500) {
-    ++micros;
-  }
-  return micros;
-}
-
-
-
 #define OAI_UL_LDPC_MAX_NUM_LLR 27000//26112 // NR_LDPC_NCOL_BG1*NR_LDPC_ZMAX = 68*384
 //#define DEBUG_CRC
 #ifdef DEBUG_CRC
