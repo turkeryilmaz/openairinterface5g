mateofloreza/oai5g

Adapt RACH procedure for large delay
- Add CL options for additional DURATION_RX_TO_TX and k2 value at UE, additional k2 at gNB
- Apply additional DURATION_RX_TO_TX at UE
- Apply the additional k2 at UE for scheduled msg3 by RAR and scheduled UL transmission by DCL. Re-compute the next Tx frame number
- Apply the additional k2 at gNB for scheduled msg3 and scheduled UL transmission. Re-compute the next Tx frame number
- Increase the RA window counter for reception of RAR
- Fix the data type for larger slot number in function get_samples_slot_timestamp
- Example for a one-way delay of 120ms: run gNB with "-P 7372800 --gnb_k2 480", run UE with "-P 7372800 -A 14745600 --ue_slot_Rx_Tx 480 --ue_k2 480"

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch.c
@@ -895,19 +895,22 @@ void nr_rx_sdu(const module_id_t gnb_mod_idP,
   }
 }
 
+extern uint16_t NTN_gNB_k2;
+
 long get_K2(NR_ServingCellConfigCommon_t *scc,NR_BWP_Uplink_t *ubwp, int time_domain_assignment, int mu) {
   DevAssert(scc);
   const NR_PUSCH_TimeDomainResourceAllocation_t *tda_list = ubwp ?
     ubwp->bwp_Common->pusch_ConfigCommon->choice.setup->pusch_TimeDomainAllocationList->list.array[time_domain_assignment]:
     scc->uplinkConfigCommon->initialUplinkBWP->pusch_ConfigCommon->choice.setup->pusch_TimeDomainAllocationList->list.array[time_domain_assignment];
   if (tda_list->k2)
-    return *tda_list->k2;
+    return *tda_list->k2+NTN_gNB_k2;
   else if (mu < 2)
-    return 1;
+    return 1+NTN_gNB_k2;
   else if (mu == 2)
-    return 2;
+    return 2+NTN_gNB_k2;
   else
-    return 3;
+    return 3+NTN_gNB_k2;
+
 }
 
 bool nr_UE_is_to_be_scheduled(module_id_t mod_id, int CC_id, int UE_id, frame_t frame, sub_frame_t slot)
@@ -1408,7 +1411,7 @@ bool nr_fr1_ulsch_preprocessor(module_id_t module_id, frame_t frame, sub_frame_t
   if (tda < 0)
     return false;
   int K2 = get_K2(scc, sched_ctrl->active_ubwp, tda, mu);
-  const int sched_frame = frame + (slot + K2 >= nr_slots_per_frame[mu]);
+  const int sched_frame = (frame + (slot + K2) / nr_slots_per_frame[mu]) % MAX_FRAME_NUMBER;
   const int sched_slot = (slot + K2) % nr_slots_per_frame[mu];
 
   if (!is_xlsch_in_slot(nr_mac->ulsch_slot_bitmap[sched_slot / 64], sched_slot))

turkeryilmaz/openairinterface5g

openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_ulsch.c
@@ -1343,6 +1343,8 @@ void handle_nr_srs_measurements(const module_id_t module_id,
   NR_SCHED_UNLOCK(&nrmac->sched_lock);
 }
 
+extern uint16_t NTN_gNB_k2;
+
 long get_K2(NR_PUSCH_TimeDomainResourceAllocationList_t *tdaList,
             int time_domain_assignment,
             int mu) {
@@ -1351,13 +1353,14 @@ long get_K2(NR_PUSCH_TimeDomainResourceAllocationList_t *tdaList,
   NR_PUSCH_TimeDomainResourceAllocation_t *tda = tdaList->list.array[time_domain_assignment];
 
   if (tda->k2)
-    return *tda->k2;
+    return *tda->k2+NTN_gNB_k2;
   else if (mu < 2)
-    return 1;
+    return 1+NTN_gNB_k2;
   else if (mu == 2)
-    return 2;
+    return 2+NTN_gNB_k2;
   else
-    return 3;
+    return 3+NTN_gNB_k2;
+
 }
 
 static bool nr_UE_is_to_be_scheduled(const NR_ServingCellConfigCommon_t *scc, int CC_id,  NR_UE_info_t* UE, frame_t frame, sub_frame_t slot, uint32_t ulsch_max_frame_inactivity)
@@ -1937,7 +1940,8 @@ static bool nr_fr1_ulsch_preprocessor(module_id_t module_id, frame_t frame, sub_
   const int temp_tda = get_ul_tda(nr_mac, scc, frame, slot);
   NR_PUSCH_TimeDomainResourceAllocationList_t *tdaList = get_ul_tdalist(current_BWP, sched_ctrl->coreset->controlResourceSetId, sched_ctrl->search_space->searchSpaceType->present, NR_RNTI_C);
   int K2 = get_K2(tdaList, temp_tda, mu);
-  const int sched_frame = (frame + (slot + K2 >= nr_slots_per_frame[mu])) & 1023;
+  //const int sched_frame = (frame + (slot + K2 >= nr_slots_per_frame[mu])) & 1023;
+  const int sched_frame = (frame + (slot + K2) / nr_slots_per_frame[mu]) % MAX_FRAME_NUMBER;
   const int sched_slot = (slot + K2) % nr_slots_per_frame[mu];
   const int tda = get_ul_tda(nr_mac, scc, sched_frame, sched_slot);
   if (tda < 0)