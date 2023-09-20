Alirezaemx/OAI

Add a 2nd gNB in CU config file and fix the instance value for RRC/F1

openair2/RRC/NR/cucp_cuup_direct.c
@@ -165,10 +165,12 @@ static int drb_config_gtpu_create(const protocol_ctxt_t *const ctxt_p,
 
 static void cucp_cuup_bearer_context_setup_direct(e1ap_bearer_setup_req_t *const req, instance_t instance, uint8_t xid)
 {
-  rrc_gNB_ue_context_t *ue_context_p = rrc_gNB_get_ue_context_by_rnti(RC.nrrrc[instance], req->rnti);
+  gNB_RRC_INST *rrc = RC.nrrrc[instance];
+  rrc_gNB_ue_context_t *ue_context_p = rrc_gNB_get_ue_context_by_rnti(rrc, req->rnti);
   gNB_RRC_UE_t *UE = &ue_context_p->ue_context;
+
   protocol_ctxt_t ctxt = {0};
-  PROTOCOL_CTXT_SET_BY_MODULE_ID(&ctxt, 0, GNB_FLAG_YES, UE->rnti, 0, 0, 0);
+  PROTOCOL_CTXT_SET_BY_MODULE_ID(&ctxt, instance, GNB_FLAG_YES, UE->rnti, 0, 0, 0);
 
   fill_DRB_configList(&ctxt, ue_context_p, xid);
   e1ap_bearer_setup_resp_t resp = {0};
@@ -185,18 +187,17 @@ static void cucp_cuup_bearer_context_setup_direct(e1ap_bearer_setup_req_t *const
     }
   }
 
-  gNB_RRC_INST *rrc = RC.nrrrc[ctxt.module_id];
   // GTP tunnel for UL
   int ret = drb_config_gtpu_create(&ctxt, ue_context_p, req, UE->DRB_configList, rrc->e1_inst);
   if (ret < 0) AssertFatal(false, "Unable to configure DRB or to create GTP Tunnel\n");
 
   // Used to store teids: if monolithic, will simply be NULL
-  if(!NODE_IS_CU(RC.nrrrc[ctxt.module_id]->node_type)) {
+  if(!NODE_IS_CU(rrc->node_type)) {
     // intentionally empty
   } else {
-    int remote_port = RC.nrrrc[ctxt.module_id]->eth_params_s.remote_portd;
-    in_addr_t my_addr = inet_addr(RC.nrrrc[ctxt.module_id]->eth_params_s.my_addr);
-    instance_t gtpInst = getCxt(CUtype, instance)->gtpInst;
+    int remote_port = rrc->eth_params_s.remote_portd;
+    in_addr_t my_addr = inet_addr(rrc->eth_params_s.my_addr);
+    instance_t gtpInst = getCxt(CUtype, rrc->f1_instance)->gtpInst;
     // GTP tunnel for DL
     fill_e1ap_bearer_setup_resp(&resp, req, gtpInst, UE->rnti, remote_port, my_addr);
   }
@@ -206,11 +207,12 @@ static void cucp_cuup_bearer_context_setup_direct(e1ap_bearer_setup_req_t *const
   prepare_and_send_ue_context_modification_f1(ue_context_p, &resp);
 }
 
-static void cucp_cuup_bearer_context_mod_direct(e1ap_bearer_setup_req_t *const req, instance_t instance, uint8_t xid) {
+static void cucp_cuup_bearer_context_mod_direct(e1ap_bearer_setup_req_t *const req, instance_t instance, uint8_t xid)
+{
   // only update GTP tunnels if it is really a CU
-  if (!NODE_IS_CU(RC.nrrrc[0]->node_type))
+  if (!NODE_IS_CU(RC.nrrrc[instance]->node_type))
     return;
-  instance_t gtpInst = getCxt(CUtype, instance)->gtpInst;
+  instance_t gtpInst = getCxt(CUtype, RC.nrrrc[instance]->f1_instance)->gtpInst;
   CU_update_UP_DL_tunnel(req, gtpInst, req->rnti);
 }
 

openair2/RRC/NR/rrc_gNB.c
@@ -159,6 +159,26 @@ static void nr_rrc_addmod_drbs(int rnti,
 ///---------------------------------------------------------------------------------------------------------------///
 ///---------------------------------------------------------------------------------------------------------------///
 
+static instance_t get_instance_by_NR_cellid(uint64_t nr_cellid)
+{
+  for (int i = 0; i < RC.nb_nr_inst; i++) {
+    if (RC.nrrrc[i] && RC.nrrrc[i]->nr_cellid == nr_cellid) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+static instance_t get_instance_by_F1_instance(instance_t f1_instance)
+{
+  for (int i = 0; i < RC.nb_nr_inst; i++) {
+    if (RC.nrrrc[i] && RC.nrrrc[i]->f1_instance == f1_instance) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 static void init_NR_SI(gNB_RRC_INST *rrc, gNB_RrcConfigurationReq *configuration)
 {
 
@@ -384,7 +404,12 @@ static void rrc_gNB_generate_RRCSetup(instance_t instance,
     .rnti = ue_p->rnti,
     .srb_id = CCCH
   };
-  rrc->mac_rrc.dl_rrc_message_transfer(instance, &dl_rrc);
+
+  int i = instance;
+  if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+    i = RC.nrrrc[instance]->f1_instance;
+  }
+  rrc->mac_rrc.dl_rrc_message_transfer(i, &dl_rrc);
 }
 
 //-----------------------------------------------------------------------------
@@ -2243,21 +2268,18 @@ void rrc_gNB_process_f1_setup_req(f1ap_setup_req_t *f1_setup_req, instance_t ins
 void rrc_gNB_process_initial_ul_rrc_message(const f1ap_initial_ul_rrc_message_t *ul_rrc)
 {
   // first get RRC instance (note, no the ITTI instance)
-  module_id_t i = 0;
-  for (i=0; i < RC.nb_nr_inst; i++) {
-    gNB_RRC_INST *rrc = RC.nrrrc[i];
-    if (rrc->nr_cellid == ul_rrc->nr_cellid)
-      break;
-  }
+  module_id_t module_id = get_instance_by_NR_cellid(ul_rrc->nr_cellid);
+
   //AssertFatal(i != RC.nb_nr_inst, "Cell_id not found\n");
   // TODO REMOVE_DU_RRC in monolithic mode, the MAC does not have the
   // nr_cellid. Thus, the above check would fail. For the time being, just put
   // a warning, as we handle one DU only anyway
-  if (i == RC.nb_nr_inst) {
-    i = 0;
+  if (module_id >= RC.nb_nr_inst) {
+    module_id = 0;
     LOG_W(RRC, "initial UL RRC message nr_cellid %ld does not match RRC's %ld\n", ul_rrc->nr_cellid, RC.nrrrc[0]->nr_cellid);
   }
-  nr_rrc_gNB_decode_ccch(i, ul_rrc->crnti, ul_rrc->rrc_container, ul_rrc->rrc_container_length, ul_rrc->du2cu_rrc_container, ul_rrc->du2cu_rrc_container_length);
+
+  nr_rrc_gNB_decode_ccch(module_id, ul_rrc->crnti, ul_rrc->rrc_container, ul_rrc->rrc_container_length, ul_rrc->du2cu_rrc_container, ul_rrc->du2cu_rrc_container_length);
 
   if (ul_rrc->rrc_container)
     free(ul_rrc->rrc_container);
@@ -2338,7 +2360,7 @@ static void rrc_CU_process_ue_context_release_complete(MessageDef *msg_p)
 static void rrc_CU_process_ue_context_modification_response(MessageDef *msg_p, instance_t instance)
 {
   f1ap_ue_context_modif_resp_t *resp = &F1AP_UE_CONTEXT_MODIFICATION_RESP(msg_p);
-  protocol_ctxt_t ctxt = {.rntiMaybeUEid = resp->rnti, .module_id = instance, .instance = instance, .enb_flag = 1, .eNB_index = instance};
+  protocol_ctxt_t ctxt = {.rntiMaybeUEid = resp->rnti, .module_id = instance, .instance = RC.nrrrc[instance]->f1_instance, .enb_flag = 1, .eNB_index = instance};
   gNB_RRC_INST *rrc = RC.nrrrc[ctxt.module_id];
   rrc_gNB_ue_context_t *ue_context_p = rrc_gNB_get_ue_context_by_rnti(rrc, resp->rnti);
   gNB_RRC_UE_t *UE = &ue_context_p->ue_context;
@@ -2746,19 +2768,24 @@ void *rrc_gnb_task(void *args_p) {
 
       /* Messages from PDCP */
       case F1AP_UL_RRC_MESSAGE:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         PROTOCOL_CTXT_SET_BY_INSTANCE(&ctxt,
                                       instance,
                                       GNB_FLAG_YES,
                                       F1AP_UL_RRC_MESSAGE(msg_p).rnti,
                                       0,
                                       0);
-        LOG_D(NR_RRC,
-              "Decoding DCCH %d: ue %04lx, inst %ld, ctxt %p, size %d\n",
-              F1AP_UL_RRC_MESSAGE(msg_p).srb_id,
-              ctxt.rntiMaybeUEid,
-              instance,
-              &ctxt,
-              F1AP_UL_RRC_MESSAGE(msg_p).rrc_container_length);
+        if (NODE_IS_CU(RC.nrrrc[ctxt.module_id]->node_type)) {
+          ctxt.instance = RC.nrrrc[ctxt.module_id]->f1_instance;
+        }
+        LOG_D(NR_RRC,"Decoding DCCH %d: ue %04lx, inst %ld, ctxt %p, size %d\n",
+                F1AP_UL_RRC_MESSAGE(msg_p).srb_id,
+                ctxt.rntiMaybeUEid,
+                instance,
+                &ctxt,
+                F1AP_UL_RRC_MESSAGE(msg_p).rrc_container_length);
         rrc_gNB_decode_dcch(&ctxt,
                             F1AP_UL_RRC_MESSAGE(msg_p).srb_id,
                             F1AP_UL_RRC_MESSAGE(msg_p).rrc_container,
@@ -2767,10 +2794,16 @@ void *rrc_gnb_task(void *args_p) {
         break;
 
       case NGAP_DOWNLINK_NAS:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_gNB_process_NGAP_DOWNLINK_NAS(msg_p, instance, &rrc_gNB_mui);
         break;
 
       case NGAP_PDUSESSION_SETUP_REQ:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_gNB_process_NGAP_PDUSESSION_SETUP_REQ(msg_p, instance);
         break;
 
@@ -2789,15 +2822,23 @@ void *rrc_gnb_task(void *args_p) {
 
       /* Messages from F1AP task */
       case F1AP_SETUP_REQ:
+        ctxt.module_id = get_instance_by_NR_cellid(F1AP_SETUP_REQ(msg_p).cell[0].nr_cellid);
+        RC.nrrrc[ctxt.module_id]->f1_instance = instance;
         AssertFatal(NODE_IS_CU(RC.nrrrc[instance]->node_type), "should not receive F1AP_SETUP_REQUEST, need call by CU!\n");
-        rrc_gNB_process_f1_setup_req(&F1AP_SETUP_REQ(msg_p));
+        rrc_gNB_process_f1_setup_req(&F1AP_SETUP_REQ(msg_p), ctxt.module_id);
         break;
 
       case F1AP_UE_CONTEXT_SETUP_RESP:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_CU_process_ue_context_setup_response(msg_p, instance);
         break;
 
       case F1AP_UE_CONTEXT_MODIFICATION_RESP:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_CU_process_ue_context_modification_response(msg_p, instance);
         break;
 
@@ -2820,6 +2861,9 @@ void *rrc_gnb_task(void *args_p) {
         break;
 
       case NGAP_INITIAL_CONTEXT_SETUP_REQ:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_gNB_process_NGAP_INITIAL_CONTEXT_SETUP_REQ(msg_p, instance);
         break;
 

turkeryilmaz/openairinterface5g

openair2/RRC/NR/cucp_cuup_direct.c
@@ -162,10 +162,12 @@ static int drb_config_gtpu_create(const protocol_ctxt_t *const ctxt_p,
 
 static void cucp_cuup_bearer_context_setup_direct(e1ap_bearer_setup_req_t *const req, instance_t instance)
 {
-  rrc_gNB_ue_context_t *ue_context_p = rrc_gNB_get_ue_context(RC.nrrrc[instance], req->gNB_cu_cp_ue_id);
+  gNB_RRC_INST *rrc = RC.nrrrc[instance];
+  rrc_gNB_ue_context_t *ue_context_p = rrc_gNB_get_ue_context(rrc, req->gNB_cu_cp_ue_id);
   gNB_RRC_UE_t *UE = &ue_context_p->ue_context;
+
   protocol_ctxt_t ctxt = {0};
-  PROTOCOL_CTXT_SET_BY_MODULE_ID(&ctxt, 0, GNB_FLAG_YES, UE->rrc_ue_id, 0, 0, 0);
+  PROTOCOL_CTXT_SET_BY_MODULE_ID(&ctxt, instance, GNB_FLAG_YES, UE->rrc_ue_id, 0, 0, 0);
 
   e1ap_bearer_setup_resp_t resp = {0};
   resp.numPDUSessions = req->numPDUSessions;
@@ -181,21 +183,20 @@ static void cucp_cuup_bearer_context_setup_direct(e1ap_bearer_setup_req_t *const
     }
   }
 
-  gNB_RRC_INST *rrc = RC.nrrrc[ctxt.module_id];
   // GTP tunnel for UL
   NR_DRB_ToAddModList_t *DRB_configList = fill_DRB_configList(UE);
   int ret = drb_config_gtpu_create(&ctxt, ue_context_p, req, DRB_configList, rrc->e1_inst);
   if (ret < 0) AssertFatal(false, "Unable to configure DRB or to create GTP Tunnel\n");
-  // the code is very badly organized, it is not possible here to call freeDRBlist() 
+  // the code is very badly organized, it is not possible here to call freeDRBlist()
   ASN_STRUCT_FREE(asn_DEF_NR_DRB_ToAddModList,DRB_configList );
 
   // Used to store teids: if monolithic, will simply be NULL
-  if(!NODE_IS_CU(RC.nrrrc[ctxt.module_id]->node_type)) {
+  if(!NODE_IS_CU(rrc->node_type)) {
     // intentionally empty
   } else {
-    int remote_port = RC.nrrrc[ctxt.module_id]->eth_params_s.remote_portd;
-    in_addr_t my_addr = inet_addr(RC.nrrrc[ctxt.module_id]->eth_params_s.my_addr);
-    instance_t gtpInst = getCxt(CUtype, instance)->gtpInst;
+    int remote_port = rrc->eth_params_s.remote_portd;
+    in_addr_t my_addr = inet_addr(rrc->eth_params_s.my_addr);
+    instance_t gtpInst = getCxt(CUtype, rrc->f1_instance)->gtpInst;
     // GTP tunnel for DL
     fill_e1ap_bearer_setup_resp(&resp, req, gtpInst, UE->rrc_ue_id, remote_port, my_addr);
   }
@@ -208,9 +209,9 @@ static void cucp_cuup_bearer_context_setup_direct(e1ap_bearer_setup_req_t *const
 static void cucp_cuup_bearer_context_mod_direct(e1ap_bearer_setup_req_t *const req, instance_t instance)
 {
   // only update GTP tunnels if it is really a CU
-  if (!NODE_IS_CU(RC.nrrrc[0]->node_type))
+  if (!NODE_IS_CU(RC.nrrrc[instance]->node_type))
     return;
-  instance_t gtpInst = getCxt(CUtype, instance)->gtpInst;
+  instance_t gtpInst = getCxt(CUtype, RC.nrrrc[instance]->f1_instance)->gtpInst;
   CU_update_UP_DL_tunnel(req, gtpInst, req->gNB_cu_cp_ue_id);
 }
 

openair2/RRC/NR/rrc_gNB.c
@@ -197,7 +197,7 @@ static void rrc_deliver_dl_rrc_message(void *deliver_pdu_data, ue_id_t ue_id, in
   data->dl_rrc->rrc_container = (uint8_t *)buf;
   data->dl_rrc->rrc_container_length = size;
   DevAssert(data->dl_rrc->srb_id == srb_id);
-  data->rrc->mac_rrc.dl_rrc_message_transfer(data->dl_rrc);
+  data->rrc->mac_rrc.dl_rrc_message_transfer(0, data->dl_rrc);
 }
 
 
@@ -212,6 +212,26 @@ void nr_rrc_transfer_protected_rrc_message(const gNB_RRC_INST *rrc, const gNB_RR
 
 ///---------------------------------------------------------------------------------------------------------------///
 ///---------------------------------------------------------------------------------------------------------------///
+
+static instance_t get_instance_by_NR_cellid(uint64_t nr_cellid)
+{
+  for (int i = 0; i < RC.nb_nr_inst; i++) {
+    if (RC.nrrrc[i] && RC.nrrrc[i]->nr_cellid == nr_cellid) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+static instance_t get_instance_by_F1_instance(instance_t f1_instance)
+{
+  for (int i = 0; i < RC.nb_nr_inst; i++) {
+    if (RC.nrrrc[i] && RC.nrrrc[i]->f1_instance == f1_instance) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 static void init_NR_SI(gNB_RRC_INST *rrc, gNB_RrcConfigurationReq *configuration)
 {
@@ -435,13 +455,18 @@ static void rrc_gNB_generate_RRCSetup(instance_t instance,
   freeSRBlist(SRBs);
   f1_ue_data_t ue_data = cu_get_f1_ue_data(ue_p->rrc_ue_id);
   f1ap_dl_rrc_message_t dl_rrc = {
-    .gNB_CU_ue_id = ue_p->rrc_ue_id,
-    .gNB_DU_ue_id = ue_data.secondary_ue,
-    .rrc_container = buf,
-    .rrc_container_length = size,
-    .srb_id = CCCH
+      .gNB_CU_ue_id = ue_p->rrc_ue_id,
+      .gNB_DU_ue_id = ue_data.secondary_ue,
+      .rrc_container = buf,
+      .rrc_container_length = size,
+      .srb_id = CCCH
   };
-  rrc->mac_rrc.dl_rrc_message_transfer(&dl_rrc);
+
+  int i = instance;
+  if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+    i = RC.nrrrc[instance]->f1_instance;
+  }
+  rrc->mac_rrc.dl_rrc_message_transfer(i, &dl_rrc);
 }
 
 static void rrc_gNB_generate_RRCReject(module_id_t module_id, rrc_gNB_ue_context_t *const ue_context_pP)
@@ -472,7 +497,7 @@ static void rrc_gNB_generate_RRCReject(module_id_t module_id, rrc_gNB_ue_context
     .execute_duplication  = 1,
     .RAT_frequency_priority_information.en_dc = 0
   };
-  rrc->mac_rrc.dl_rrc_message_transfer(&dl_rrc);
+  rrc->mac_rrc.dl_rrc_message_transfer(0, &dl_rrc);
 }
 
 //-----------------------------------------------------------------------------
@@ -1947,21 +1972,17 @@ void rrc_gNB_process_f1_setup_req(f1ap_setup_req_t *f1_setup_req, instance_t ins
 void rrc_gNB_process_initial_ul_rrc_message(const f1ap_initial_ul_rrc_message_t *ul_rrc)
 {
   // first get RRC instance (note, no the ITTI instance)
-  module_id_t i = 0;
-  for (i=0; i < RC.nb_nr_inst; i++) {
-    gNB_RRC_INST *rrc = RC.nrrrc[i];
-    if (rrc->nr_cellid == ul_rrc->nr_cellid)
-      break;
-  }
+  module_id_t module_id = get_instance_by_NR_cellid(ul_rrc->nr_cellid);
+
   //AssertFatal(i != RC.nb_nr_inst, "Cell_id not found\n");
   // TODO REMOVE_DU_RRC in monolithic mode, the MAC does not have the
   // nr_cellid. Thus, the above check would fail. For the time being, just put
   // a warning, as we handle one DU only anyway
-  if (i == RC.nb_nr_inst) {
-    i = 0;
+  if (module_id >= RC.nb_nr_inst) {
+    module_id = 0;
     LOG_W(RRC, "initial UL RRC message nr_cellid %ld does not match RRC's %ld\n", ul_rrc->nr_cellid, RC.nrrrc[0]->nr_cellid);
   }
-  nr_rrc_gNB_decode_ccch(i, ul_rrc);
+  nr_rrc_gNB_decode_ccch(module_id, ul_rrc);
 
   if (ul_rrc->rrc_container)
     free(ul_rrc->rrc_container);
@@ -2054,7 +2075,7 @@ static void rrc_CU_process_ue_context_release_complete(MessageDef *msg_p)
 static void rrc_CU_process_ue_context_modification_response(MessageDef *msg_p, instance_t instance)
 {
   f1ap_ue_context_modif_resp_t *resp = &F1AP_UE_CONTEXT_MODIFICATION_RESP(msg_p);
-  protocol_ctxt_t ctxt = {.rntiMaybeUEid = resp->gNB_CU_ue_id, .module_id = instance, .instance = instance, .enb_flag = 1, .eNB_index = instance};
+  protocol_ctxt_t ctxt = {.rntiMaybeUEid = resp->gNB_CU_ue_id, .module_id = instance, .instance = RC.nrrrc[instance]->f1_instance, .enb_flag = 1, .eNB_index = instance};
   gNB_RRC_INST *rrc = RC.nrrrc[ctxt.module_id];
   rrc_gNB_ue_context_t *ue_context_p = rrc_gNB_get_ue_context(rrc, resp->gNB_CU_ue_id);
   if (!ue_context_p) {
@@ -2581,19 +2602,24 @@ void *rrc_gnb_task(void *args_p) {
 
       /* Messages from PDCP */
       case F1AP_UL_RRC_MESSAGE:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         PROTOCOL_CTXT_SET_BY_INSTANCE(&ctxt,
                                       instance,
                                       GNB_FLAG_YES,
                                       F1AP_UL_RRC_MESSAGE(msg_p).gNB_CU_ue_id,
                                       0,
                                       0);
-        LOG_D(NR_RRC,
-              "Decoding DCCH %d: ue %04lx, inst %ld, ctxt %p, size %d\n",
-              F1AP_UL_RRC_MESSAGE(msg_p).srb_id,
-              ctxt.rntiMaybeUEid,
-              instance,
-              &ctxt,
-              F1AP_UL_RRC_MESSAGE(msg_p).rrc_container_length);
+        if (NODE_IS_CU(RC.nrrrc[ctxt.module_id]->node_type)) {
+          ctxt.instance = RC.nrrrc[ctxt.module_id]->f1_instance;
+        }
+        LOG_D(NR_RRC,"Decoding DCCH %d: ue %04lx, inst %ld, ctxt %p, size %d\n",
+                F1AP_UL_RRC_MESSAGE(msg_p).srb_id,
+                ctxt.rntiMaybeUEid,
+                instance,
+                &ctxt,
+                F1AP_UL_RRC_MESSAGE(msg_p).rrc_container_length);
         rrc_gNB_decode_dcch(&ctxt,
                             F1AP_UL_RRC_MESSAGE(msg_p).srb_id,
                             F1AP_UL_RRC_MESSAGE(msg_p).rrc_container,
@@ -2602,10 +2628,16 @@ void *rrc_gnb_task(void *args_p) {
         break;
 
       case NGAP_DOWNLINK_NAS:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_gNB_process_NGAP_DOWNLINK_NAS(msg_p, instance, &rrc_gNB_mui);
         break;
 
       case NGAP_PDUSESSION_SETUP_REQ:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_gNB_process_NGAP_PDUSESSION_SETUP_REQ(msg_p, instance);
         break;
 
@@ -2624,15 +2656,23 @@ void *rrc_gnb_task(void *args_p) {
 
       /* Messages from F1AP task */
       case F1AP_SETUP_REQ:
+        ctxt.module_id = get_instance_by_NR_cellid(F1AP_SETUP_REQ(msg_p).cell[0].nr_cellid);
+        RC.nrrrc[ctxt.module_id]->f1_instance = instance;
         AssertFatal(NODE_IS_CU(RC.nrrrc[instance]->node_type), "should not receive F1AP_SETUP_REQUEST, need call by CU!\n");
-        rrc_gNB_process_f1_setup_req(&F1AP_SETUP_REQ(msg_p));
+        rrc_gNB_process_f1_setup_req(&F1AP_SETUP_REQ(msg_p), ctxt.module_id);
         break;
 
       case F1AP_UE_CONTEXT_SETUP_RESP:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_CU_process_ue_context_setup_response(msg_p, instance);
         break;
 
       case F1AP_UE_CONTEXT_MODIFICATION_RESP:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_CU_process_ue_context_modification_response(msg_p, instance);
         break;
 
@@ -2659,6 +2699,9 @@ void *rrc_gnb_task(void *args_p) {
         break;
 
       case NGAP_INITIAL_CONTEXT_SETUP_REQ:
+        if (NODE_IS_CU(RC.nrrrc[instance]->node_type)) {
+          instance = get_instance_by_F1_instance(instance);
+        }
         rrc_gNB_process_NGAP_INITIAL_CONTEXT_SETUP_REQ(msg_p, instance);
         break;
 