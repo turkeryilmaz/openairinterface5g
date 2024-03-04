aa695207a7a8965ef1f48b6ab20820a805b69267
Teodora <teodora.vladic@openairinterface.org>
2024-03-04 14:36:49 +0100

    RC Control RAN Function Definition implemented

--- openair2/E2AP/RAN_FUNCTION/O-RAN/ran_func_rc.c
+++ openair2/E2AP/RAN_FUNCTION/O-RAN/ran_func_rc.c
@@ -22,7 +22,6 @@
 #include "ran_func_rc.h"
 #include "ran_func_rc_subs.h"
 #include "ran_func_rc_extern.h"
-#include "../../flexric/test/rnd/fill_rnd_data_rc.h"  // this dependancy will be taken out once RAN Function Definition is implemented
 #include "../../flexric/src/sm/rc_sm/ie/ir/lst_ran_param.h"
 #include "../../flexric/src/sm/rc_sm/ie/ir/ran_param_list.h"
 #include "../../flexric/src/agent/e2_agent_api.h"
@@ -35,17 +34,437 @@ static pthread_once_t once_rc_mutex = PTHREAD_ONCE_INIT;
 static rc_subs_data_t rc_subs_data = {0};
 static pthread_mutex_t rc_mutex = PTHREAD_MUTEX_INITIALIZER;
 
+static ngran_node_t node_type = 0;
+static void get_e2_node_type(void)
+{
+  #if defined (NGRAN_GNB_DU) && defined (NGRAN_GNB_CUUP) && defined (NGRAN_GNB_CUCP)
+  node_type = RC.nrrrc[0]->node_type;
+  #elif defined (NGRAN_GNB_CUUP)
+  node_type =  ngran_gNB_CUUP;
+  #endif
+}
+
 static void init_once_rc(void)
 {
   init_rc_subs_data(&rc_subs_data);
 }
+
+static void fill_rc_ev_trig(ran_func_def_ev_trig_t* ev_trig)
+{
+  // Sequence of EVENT TRIGGER styles
+  // [1 - 63]
+  ev_trig->sz_seq_ev_trg_style = 1;
+  ev_trig->seq_ev_trg_style = calloc(ev_trig->sz_seq_ev_trg_style, sizeof(seq_ev_trg_style_t));
+  assert(ev_trig->seq_ev_trg_style != NULL && "Memory exhausted");
+
+  seq_ev_trg_style_t* ev_trig_style = &ev_trig->seq_ev_trg_style[0];
+
+  // RIC Event Trigger Style Type
+  // Mandatory
+  // 9.3.3
+  // 6.2.2.2.
+  //  INTEGER
+  ev_trig_style->style = 4;
+
+  // RIC Event Trigger Style Name
+  // Mandatory
+  // 9.3.4
+  // 6.2.2.3
+  //PrintableString(SIZE(1..150,...))
+  const char ev_style_name[] = "UE Information Change";
+  ev_trig_style->name = cp_str_to_ba(ev_style_name);
+
+  // RIC Event Trigger Format Type
+  // Mandatory
+  // 9.3.5
+  // 6.2.2.4.
+  // INTEGER
+  ev_trig_style->format = FORMAT_4_E2SM_RC_EV_TRIGGER_FORMAT;
+
+  // Sequence of RAN Parameters for L2 Variables
+  // [0 - 65535]
+  ev_trig->sz_seq_ran_param_l2_var = 0;
+  ev_trig->seq_ran_param_l2_var = NULL;
+
+  //Sequence of Call Process Types
+  // [0-65535]
+  ev_trig->sz_seq_call_proc_type = 0;
+  ev_trig->seq_call_proc_type = NULL;
+
+  // Sequence of RAN Parameters for Identifying UEs
+  // 0-65535
+  ev_trig->sz_seq_ran_param_id_ue = 0;
+  ev_trig->seq_ran_param_id_ue = NULL;
+
+  // Sequence of RAN Parameters for Identifying Cells
+  // 0-65535
+  ev_trig->sz_seq_ran_param_id_cell = 0;
+  ev_trig->seq_ran_param_id_cell = NULL;
+}
+
+static void fill_rc_report(ran_func_def_report_t* report)
+{
+  // Sequence of REPORT styles
+  // [1 - 63]
+  report->sz_seq_report_sty = 1;
+  report->seq_report_sty = calloc(report->sz_seq_report_sty, sizeof(seq_report_sty_t));
+  assert(report->seq_report_sty != NULL && "Memory exhausted");
+
+  seq_report_sty_t* report_style = &report->seq_report_sty[0];
+
+    // RIC Report Style Type
+  // Mandatory
+  // 9.3.3
+  // 6.2.2.2.
+  // INTEGER
+  report_style->report_type = 4;
+
+  // RIC Report Style Name
+  // Mandatory
+  // 9.3.4
+  // 6.2.2.3.
+  // PrintableString(SIZE(1..150,...)) 
+  const char report_name[] = "UE Information";
+  report_style->name = cp_str_to_ba(report_name);
+
+  // Supported RIC Event Trigger Style Type 
+  // Mandatory
+  // 9.3.3
+  // 6.2.2.2.
+  // INTEGER
+  report_style->ev_trig_type = FORMAT_4_E2SM_RC_EV_TRIGGER_FORMAT;
+
+  // RIC Report Action Format Type
+  // Mandatory
+  // 9.3.5
+  // 6.2.2.4.
+  // INTEGER
+  report_style->act_frmt_type = FORMAT_1_E2SM_RC_ACT_DEF;
+
+  // RIC Indication Header Format Type
+  // Mandatory
+  // 9.3.5
+  // 6.2.2.4.
+  // INTEGER
+  report_style->ind_hdr_type = FORMAT_1_E2SM_RC_IND_HDR;
+
+  // RIC Indication Message Format Type
+  // Mandatory
+  // 9.3.5
+  // 6.2.2.4.
+  // INTEGER
+  report_style->ind_msg_type = FORMAT_2_E2SM_RC_IND_MSG;
+
+  // Sequence of RAN Parameters Supported
+  // [0 - 65535]
+  report_style->sz_seq_ran_param = 1;
+  report_style->ran_param = calloc(report_style->sz_seq_ran_param, sizeof(seq_ran_param_3_t));
+  assert(report_style->ran_param != NULL && "Memory exhausted");
+
+  // RAN Parameter ID
+  // Mandatory
+  // 9.3.8
+  // [1- 4294967295]
+  report_style->ran_param[0].id = RRC_STATE_CHANGED_TO_E2SM_RC_RAN_PARAM_ID;
+
+  // RAN Parameter Name
+  // Mandatory
+  // 9.3.9
+  // [1-150] 
+  const char ran_param_name[] = "RRC State";
+  report_style->ran_param[0].name = cp_str_to_ba(ran_param_name);
+
+  // RAN Parameter Definition
+  // Optional
+  // 9.3.51
+  report_style->ran_param[0].def = NULL;
+}
+
+static void fill_rc_control(ran_func_def_ctrl_t* ctrl)
+{
+  // Sequence of CONTROL styles
+  // [1 - 63]
+  ctrl->sz_seq_ctrl_style = 1;
+  ctrl->seq_ctrl_style = calloc(ctrl->sz_seq_ctrl_style, sizeof(seq_ctrl_style_t));
+  assert(ctrl->seq_ctrl_style != NULL && "Memory exhausted");
+
+  seq_ctrl_style_t* ctrl_style = &ctrl->seq_ctrl_style[0];
+
+  // RIC Control Style Type
+  // Mandatory
+  // 9.3.3
+  // 6.2.2.2
+  ctrl_style->style_type = 1;
+
+  //RIC Control Style Name
+  //Mandatory
+  //9.3.4
+  // [1 -150]
+  const char control_name[] = "Radio Bearer Control";
+  ctrl_style->name = cp_str_to_ba(control_name);
+
+  // RIC Control Header Format Type
+  // Mandatory
+  // 9.3.5
+  ctrl_style->hdr = FORMAT_1_E2SM_RC_CTRL_HDR;
+
+  // RIC Control Message Format Type
+  // Mandatory
+  // 9.3.5
+  ctrl_style->msg = FORMAT_1_E2SM_RC_CTRL_MSG;
+
+  // RIC Call Process ID Format Type
+  // Optional
+  ctrl_style->call_proc_id_type = NULL;
+
+  // RIC Control Outcome Format Type
+  // Mandatory
+  // 9.3.5
+  ctrl_style->out_frmt = FORMAT_1_E2SM_RC_CTRL_OUT;
+
+  // Sequence of Control Actions
+  // [0-65535]
+  ctrl_style->sz_seq_ctrl_act = 1;
+  ctrl_style->seq_ctrl_act = calloc(ctrl_style->sz_seq_ctrl_act, sizeof(seq_ctrl_act_2_t));
+  assert(ctrl_style->seq_ctrl_act != NULL && "Memory exhausted");
+
+  seq_ctrl_act_2_t* ctrl_act = &ctrl_style->seq_ctrl_act[0];
+
+  // Control Action ID
+  // Mandatory
+  // 9.3.6
+  // [1-65535]
+  ctrl_act->id = 2;
+
+  // Control Action Name
+  // Mandatory
+  // 9.3.7
+  // [1-150]
+  const char control_act_name[] = "QoS flow mapping configuration";
+  ctrl_act->name = cp_str_to_ba(control_act_name);
+
+  // Sequence of Associated RAN Parameters
+  // [0-65535]
+  ctrl_act->sz_seq_assoc_ran_param = 2;
+  ctrl_act->assoc_ran_param = calloc(ctrl_act->sz_seq_assoc_ran_param, sizeof(seq_ran_param_3_t));
+  assert(ctrl_act->assoc_ran_param != NULL && "Memory exhausted");
+
+  seq_ran_param_3_t* assoc_ran_param = ctrl_act->assoc_ran_param;
+
+  // DRB ID
+  assoc_ran_param[0].id = 1;
+  const char ran_param_drb_id[] = "DRB ID";
+  assoc_ran_param[0].name = cp_str_to_ba(ran_param_drb_id);
+  assoc_ran_param[0].def = NULL;
+
+  // List of QoS Flows to be modified in DRB
+  assoc_ran_param[1].id = 2;
+  const char ran_param_list_qos_flow[] = "List of QoS Flows to be modified in DRB";
+  assoc_ran_param[1].name = cp_str_to_ba(ran_param_list_qos_flow);
+  assoc_ran_param[1].def = calloc(1, sizeof(ran_param_def_t));
+  assert(assoc_ran_param[1].def != NULL && "Memory exhausted");
+
+  assoc_ran_param[1].def->type = LIST_RAN_PARAMETER_DEF_TYPE;
+  assoc_ran_param[1].def->lst = calloc(1, sizeof(ran_param_type_t));
+  assert(assoc_ran_param[1].def->lst != NULL && "Memory exhausted");
+
+  ran_param_type_t* lst = assoc_ran_param[1].def->lst;
+  lst->sz_ran_param = 2;
+  lst->ran_param = calloc(lst->sz_ran_param, sizeof(ran_param_lst_struct_t));
+  assert(lst->ran_param != NULL && "Memory exhausted");
+
+  // QoS Flow Identifier
+  lst->ran_param[0].ran_param_id = 4;
+  const char ran_param_qos_flow_id[] = "QoS Flow Identifier";
+  lst->ran_param[0].ran_param_name = cp_str_to_ba(ran_param_qos_flow_id);
+  lst->ran_param[0].ran_param_def = NULL;
+
+  // QoS Flow Mapping Indication
+  lst->ran_param[1].ran_param_id = 5;
+  const char ran_param_qos_flow_mapping_ind[] = "QoS Flow Mapping Indication";
+  lst->ran_param[1].ran_param_name = cp_str_to_ba(ran_param_qos_flow_mapping_ind);
+  lst->ran_param[1].ran_param_def = NULL;
+
+  // Sequence of Associated RAN 
+  // Parameters for Control Outcome
+  // [0- 255]
+  ctrl_style->sz_ran_param_ctrl_out = 0;
+  ctrl_style->ran_param_ctrl_out = NULL;
+}
+
+e2sm_rc_func_def_t fill_rc_ran_def_gnb(void)
+{
+  e2sm_rc_func_def_t def = {0};
+
+  // RAN Function Definition for EVENT TRIGGER
+  // Optional
+  // 9.2.2.2
+  def.ev_trig = calloc(1, sizeof(ran_func_def_ev_trig_t));
+  assert(def.ev_trig != NULL && "Memory exhausted");
+  fill_rc_ev_trig(def.ev_trig);
+
+  // RAN Function Definition for REPORT
+  // Optional
+  // 9.2.2.3
+  def.report = calloc(1, sizeof(ran_func_def_report_t));
+  assert(def.report != NULL && "Memory exhausted");
+  fill_rc_report(def.report);
+
+  // RAN Function Definition for INSERT
+  // Optional
+  // 9.2.2.4
+  def.insert = NULL;
+
+  // RAN Function Definition for CONTROL
+  // Optional
+  // 9.2.2.5
+  def.ctrl = calloc(1, sizeof(ran_func_def_ctrl_t));
+  assert(def.ctrl != NULL && "Memory exhausted");
+  fill_rc_control(def.ctrl);
+
+  // RAN Function Definition for POLICY
+  // Optional
+  // 9.2.2.6
+  def.policy = NULL;
+
+  return def;
+}
+
+static e2sm_rc_func_def_t fill_rc_ran_def_cu(void)
+{
+  e2sm_rc_func_def_t def = {0};
+
+  // RAN Function Definition for EVENT TRIGGER
+  // Optional
+  // 9.2.2.2
+  def.ev_trig = calloc(1, sizeof(ran_func_def_ev_trig_t));
+  assert(def.ev_trig != NULL && "Memory exhausted");
+  fill_rc_ev_trig(def.ev_trig);
+
+  // RAN Function Definition for REPORT
+  // Optional
+  // 9.2.2.3
+  def.report = calloc(1, sizeof(ran_func_def_report_t));
+  assert(def.report != NULL && "Memory exhausted");
+  fill_rc_report(def.report);
+
+  // RAN Function Definition for INSERT
+  // Optional
+  // 9.2.2.4
+  def.insert = NULL;
+
+  // RAN Function Definition for CONTROL
+  // Optional
+  // 9.2.2.5
+  def.ctrl = calloc(1, sizeof(ran_func_def_ctrl_t));
+  assert(def.ctrl != NULL && "Memory exhausted");
+  fill_rc_control(def.ctrl);
+
+  // RAN Function Definition for POLICY
+  // Optional
+  // 9.2.2.6
+  def.policy = NULL;
+
+  return def;
+}
+
+static e2sm_rc_func_def_t fill_rc_ran_def_null(void)
+{
+  e2sm_rc_func_def_t def = {0};
+
+  // RAN Function Definition for EVENT TRIGGER
+  // Optional
+  // 9.2.2.2
+  def.ev_trig = NULL;
+
+  // RAN Function Definition for REPORT
+  // Optional
+  // 9.2.2.3
+  def.report = NULL;
+
+  // RAN Function Definition for INSERT
+  // Optional
+  // 9.2.2.4
+  def.insert = NULL;
+
+  // RAN Function Definition for CONTROL
+  // Optional
+  // 9.2.2.5
+  def.ctrl = NULL;
+
+  // RAN Function Definition for POLICY
+  // Optional
+  // 9.2.2.6
+  def.policy = NULL;
+
+  return def;
+}
+
+static e2sm_rc_func_def_t fill_rc_ran_def_cucp(void)
+{
+  e2sm_rc_func_def_t def = {0};
+
+  // RAN Function Definition for EVENT TRIGGER
+  // Optional
+  // 9.2.2.2
+  def.ev_trig = calloc(1, sizeof(ran_func_def_ev_trig_t));
+  assert(def.ev_trig != NULL && "Memory exhausted");
+  fill_rc_ev_trig(def.ev_trig);
+
+  // RAN Function Definition for REPORT
+  // Optional
+  // 9.2.2.3
+  def.report = calloc(1, sizeof(ran_func_def_report_t));
+  assert(def.report != NULL && "Memory exhausted");
+  fill_rc_report(def.report);
+
+  // RAN Function Definition for INSERT
+  // Optional
+  // 9.2.2.4
+  def.insert = NULL;
+
+  // RAN Function Definition for CONTROL
+  // Optional
+  // 9.2.2.5
+  def.ctrl = NULL;
+
+  // RAN Function Definition for POLICY
+  // Optional
+  // 9.2.2.6
+  def.policy = NULL;
+
+  return def;
+}
+
+typedef e2sm_rc_func_def_t (*fp_rc_func_def)(void);
+
+static const fp_rc_func_def ran_def_rc[END_NGRAN_NODE_TYPE] =
+{
+  NULL,
+  NULL,
+  fill_rc_ran_def_gnb,
+  NULL,
+  NULL,
+  fill_rc_ran_def_cu,
+  NULL,
+  fill_rc_ran_def_null,
+  NULL,
+  fill_rc_ran_def_cucp,
+  fill_rc_ran_def_null,
+};
 
 void read_rc_setup_sm(void* data)
 {
   assert(data != NULL);
 //  assert(data->type == RAN_CTRL_V1_3_AGENT_IF_E2_SETUP_ANS_V0);
   rc_e2_setup_t* rc = (rc_e2_setup_t*)data;
-  rc->ran_func_def = fill_rc_ran_func_def();
+
+  /* Fill the RAN Function Definition with currently supported measurements */
+  
+  // RAN Function Name is already filled in fill_ran_function_name() in rc_sm_agent.c
+
+  get_e2_node_type();
+  rc->ran_func_def = ran_def_rc[node_type]();
 
   // E2 Setup Request is sent periodically until the connection is established
   // RC subscritpion data should be initialized only once
