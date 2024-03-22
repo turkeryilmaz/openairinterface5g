b61cd39c312ffa5646c9b47824091e0d0c13a182
francescomani <email@francescomani.it>
2024-03-22 16:13:34 +0100

    fix for going to idle: release MAC config only after resetting MAC

--- openair2/LAYER2/NR_MAC_UE/config_ue.c
+++ openair2/LAYER2/NR_MAC_UE/config_ue.c
@@ -1601,16 +1601,15 @@ static void configure_timeAlignmentTimer(NR_timer_t *time_alignment_timer, NR_Ti
     nr_timer_start(time_alignment_timer);
 }
 
-void nr_rrc_mac_config_req_reset(module_id_t module_id,
-                                 NR_UE_MAC_reset_cause_t cause)
+void nr_rrc_mac_config_req_reset(module_id_t module_id, NR_UE_MAC_reset_cause_t cause)
 {
   NR_UE_MAC_INST_t *mac = get_mac_inst(module_id);
   fapi_nr_synch_request_t sync_req = {.target_Nid_cell = -1, .ssb_bw_scan = true};
   switch (cause) {
     case GO_TO_IDLE:
       reset_ra(mac, true);
-      release_mac_configuration(mac, cause);
       nr_ue_init_mac(mac);
+      release_mac_configuration(mac, cause);
       nr_ue_mac_default_configs(mac);
       // new sync but no target cell id -> -1
       nr_ue_send_synch_request(mac, module_id, 0, &sync_req);
