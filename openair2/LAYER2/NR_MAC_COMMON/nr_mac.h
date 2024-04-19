c534570eeccc4fd8704519d7276c221276bb08f2
Raymond Knopp <raymond.knopp@eurecom.fr>
2024-04-19 16:27:32 +0200

    added srcid to nas_config for SL

--- openair2/LAYER2/NR_MAC_COMMON/nr_mac.h
+++ openair2/LAYER2/NR_MAC_COMMON/nr_mac.h
@@ -139,31 +139,6 @@ static inline int get_mac_len(uint8_t* pdu, int pdu_len, uint16_t *mac_ce_len, u
   return true;
 }
 
-static inline int get_mac_len_sl(uint8_t* pdu, int pdu_len, uint16_t *mac_ce_len, uint16_t *mac_subheader_len) {
-  uint8_t sl_sch_header_len = (int)sizeof(NR_SLSCH_MAC_SUBHEADER_FIXED);
-  if (pdu_len < sl_sch_header_len)
-    return false;
-  NR_SLSCH_MAC_SUBHEADER_FIXED *sl_sch_subheader = (NR_SLSCH_MAC_SUBHEADER_FIXED*) pdu;
-  pdu += sl_sch_header_len;
-  if ( pdu_len < sl_sch_header_len + (int)sizeof(NR_MAC_SUBHEADER_SHORT))
-    return false;
-  NR_MAC_SUBHEADER_SHORT *s = (NR_MAC_SUBHEADER_SHORT*) pdu;
-  NR_MAC_SUBHEADER_LONG *l = (NR_MAC_SUBHEADER_LONG*) pdu;
-  if (s->F && pdu_len < sl_sch_header_len + (int)sizeof(NR_MAC_SUBHEADER_LONG))
-    return false;
-  if (s->F) {
-    *mac_subheader_len = sizeof(*l) + sizeof(*sl_sch_subheader);
-    *mac_ce_len = ntohs(l->L);
-  } else {
-    *mac_subheader_len = sizeof(*s) + sizeof(*sl_sch_subheader);
-    *mac_ce_len = s->L;
-  }
-  LOG_D(NR_MAC, "V %hhu, SRC %hu, DST %hhu\n", sl_sch_subheader->V, sl_sch_subheader->SRC, sl_sch_subheader->DST);
-  LOG_D(NR_MAC, "R %hhu, F %hhu, LCID %hhu, L %hu\n", l->R, l->F, l->LCID, ntohs(l->L));
-  LOG_D(NR_MAC, "mac_subheader_len %hu, mac_ce_len %hu\n", *mac_subheader_len, *mac_ce_len);
-  return true;
-}
-    
 // SL BSR MAC CEs
 // TS 38.321 ch. 6.1.3.33
 // Short BSR for a specific logical channel group ID
