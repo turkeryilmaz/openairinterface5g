c09ad1182c13b767b7160178e7a29922e9227157
francescomani <email@francescomani.it>
2024-06-04 09:13:04 +0200

    moving beamforming matrix to MAC and feeding it to FAPI structures

--- openair2/LAYER2/NR_MAC_gNB/config.c
+++ openair2/LAYER2/NR_MAC_gNB/config.c
@@ -272,7 +272,13 @@ nfapi_nr_pm_list_t init_DL_MIMO_codebook(gNB_MAC_INST *gNB, nr_pdsch_AntennaPort
   return mat;
 }
 
-static void config_common(gNB_MAC_INST *nrmac, nr_pdsch_AntennaPorts_t pdsch_AntennaPorts, int pusch_AntennaPorts, NR_ServingCellConfigCommon_t *scc)
+static void config_common(gNB_MAC_INST *nrmac,
+                          nr_pdsch_AntennaPorts_t pdsch_AntennaPorts,
+                          int pusch_AntennaPorts,
+                          int nb_tx,
+                          int nb_beams,
+                          int32_t *bw_list,
+                          NR_ServingCellConfigCommon_t *scc)
 {
   nfapi_nr_config_request_scf_t *cfg = &nrmac->config[0];
   nrmac->common_channels[0].ServingCellConfigCommon = scc;
@@ -591,6 +597,24 @@ static void config_common(gNB_MAC_INST *nrmac, nr_pdsch_AntennaPorts_t pdsch_Ant
 
   // precoding matrix configuration (to be improved)
   cfg->pmi_list = init_DL_MIMO_codebook(nrmac, pdsch_AntennaPorts);
+  // beamforming matrix configuration
+  cfg->dbt_config.num_dig_beams = nb_beams;
+  if (nb_beams > 0) {
+    cfg->dbt_config.num_txrus = nb_tx;
+    cfg->dbt_config.dig_beam_list = malloc16(nb_beams * sizeof(*cfg->dbt_config.dig_beam_list));
+    AssertFatal(cfg->dbt_config.dig_beam_list, "out of memory\n");
+    for (int i = 0; i < nb_beams; i++) {
+      nfapi_nr_dig_beam_t *beam = &cfg->dbt_config.dig_beam_list[i];
+      beam->beam_idx = i;
+      beam->txru_list = malloc16(nb_tx * sizeof(*beam->txru_list));
+      for (int j = 0; j < nb_tx; j++) {
+        nfapi_nr_txru_t *txru = &beam->txru_list[j];
+        txru->dig_beam_weight_Re = bw_list[j + i * nb_tx] & 0xffff;
+        txru->dig_beam_weight_Im = (bw_list[j + i * nb_tx] >> 16) & 0xffff;
+        LOG_D(NR_MAC, "Beam %d Tx %d Weight (%d, %d)\n", i, j, txru->dig_beam_weight_Re, txru->dig_beam_weight_Im);
+      }
+    }
+  }
 }
 
 static void initialize_beam_information(NR_beam_info_t *beam_info, int mu, int slots_per_frame)
@@ -644,7 +668,9 @@ void nr_mac_config_scc(gNB_MAC_INST *nrmac, NR_ServingCellConfigCommon_t *scc, c
 
   LOG_I(NR_MAC, "Configuring common parameters from NR ServingCellConfig\n");
 
-  config_common(nrmac, config->pdsch_AntennaPorts, config->pusch_AntennaPorts, scc);
+  int nb_tx = config->nb_bfw[0]; // number of tx antennas
+  int nb_beams = config->nb_bfw[1]; // number of beams
+  config_common(nrmac, config->pdsch_AntennaPorts, config->pusch_AntennaPorts, nb_tx, nb_beams, config->bw_list, scc);
 
   if (NFAPI_MODE == NFAPI_MONOLITHIC) {
     // nothing to be sent in the other cases
