chinchans/oai5g

Harmonize UE IP interface creation
1. NAS thread creates TUN interface & socket, configures IP and starts
SDAP thread to read from socket.
noS1 mode: RRC sends itti msg to NAS thread with default IP config.
SA mode: NAS receives configuration from core network.
noS1 mode (gNB): Move TUN read thread to SDAP in gNB.
2. Drop netlink from function name.
3. Remove hardcoding of default PDU id. Provided command line option.

openair1/SIMULATION/ETH_TRANSPORT/netlink_init.c
@@ -132,20 +132,24 @@ int netlink_init_mbms_tun(char *ifprefix, int id) {//for UE, id = 1, 2, ...,
   return 1;
 }
 
-int netlink_init_tun(char *ifprefix, int num_if, int id) {//for UE, id = 1, 2, ...,
+int init_single_tun(char *ifname)
+{
+  int sock = tun_alloc(ifname);
+  AssertFatal(sock != -1, "TUN: Error opening socket %s (%d:%s)\n", ifname, errno, strerror(errno));
+  LOG_I(OIP, "TUN: Opened interface %s with fd = %d\n", ifname, sock);
+  return sock;
+}
+
+int init_tun(char *ifprefix, int num_if, int id)
+{ // for UE, id = 1, 2, ...,
   int ret;
   char ifname[64];
 
   int begx = (id == 0) ? 0 : id - 1;
   int endx = (id == 0) ? num_if : id;
   for (int i = begx; i < endx; i++) {
-    sprintf(ifname, "oaitun_%.3s%d",ifprefix,i+1);
-    nas_sock_fd[i] = tun_alloc(ifname);
-
-    if (nas_sock_fd[i] == -1) {
-      LOG_E(PDCP, "TUN: Error opening socket %s (%d:%s)\n",ifname,errno, strerror(errno));
-      exit(1);
-    }
+    sprintf(ifname, "oaitun_%.3s%d", ifprefix, i + 1);
+    nas_sock_fd[i] = init_single_tun(ifname);
 
     LOG_I(PDCP, "TUN: Opened socket %s with fd nas_sock_fd[%d]=%d\n",
            ifname, i, nas_sock_fd[i]);
@@ -162,7 +166,6 @@ int netlink_init_tun(char *ifprefix, int num_if, int id) {//for UE, id = 1, 2, .
 
   return 1;
 }
-
 
 int netlink_init(void) {
   int ret;

turkeryilmaz/openairinterface5g

openair1/SIMULATION/ETH_TRANSPORT/netlink_init.c
@@ -132,20 +132,26 @@ int netlink_init_mbms_tun(char *ifprefix, int id) {//for UE, id = 1, 2, ...,
   return 1;
 }
 
-int netlink_init_tun(char *ifprefix, int num_if, int id) {//for UE, id = 1, 2, ...,
+int init_single_tun(char *ifname)
+{
+  int sock = tun_alloc(ifname);
+  if (sock == -1)
+    LOG_E(PDCP, "TUN: Error opening socket %s (%d:%s)\n", ifname, errno, strerror(errno));
+  else
+    LOG_I(PDCP, "TUN: Opened interface %s with fd = %d\n", ifname, sock);
+  return sock;
+}
+
+int init_tun(char *ifprefix, int num_if, int id)
+{ // for UE, id = 1, 2, ...,
   int ret;
   char ifname[64];
 
   int begx = (id == 0) ? 0 : id - 1;
   int endx = (id == 0) ? num_if : id;
   for (int i = begx; i < endx; i++) {
-    sprintf(ifname, "oaitun_%.3s%d",ifprefix,i+1);
-    nas_sock_fd[i] = tun_alloc(ifname);
-
-    if (nas_sock_fd[i] == -1) {
-      LOG_E(PDCP, "TUN: Error opening socket %s (%d:%s)\n",ifname,errno, strerror(errno));
-      return 0;
-    }
+    sprintf(ifname, "oaitun_%.3s%d", ifprefix, i + 1);
+    nas_sock_fd[i] = init_single_tun(ifname);
 
     LOG_I(PDCP, "TUN: Opened socket %s with fd nas_sock_fd[%d]=%d\n",
            ifname, i, nas_sock_fd[i]);
@@ -162,7 +168,6 @@ int netlink_init_tun(char *ifprefix, int num_if, int id) {//for UE, id = 1, 2, .
 
   return 1;
 }
-
 
 int netlink_init(void) {
   int ret;