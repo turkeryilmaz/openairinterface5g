c3db000dbffa80bf0db756c7176e4399fdcddd66
Laurent THOMAS <laurent.thomas@open-cells.com>
2024-08-12 15:14:50 +0200

    process registration reject in UE

--- openair3/NAS/NR_UE/nr_nas_msg_sim.c
+++ openair3/NAS/NR_UE/nr_nas_msg_sim.c
@@ -1448,6 +1448,10 @@ void *nas_nrue(void *args_p)
 	case FGS_PDU_SESSION_ESTABLISHMENT_REJ:
 	  LOG_E(NAS, "Received PDU Session Establishment reject\n");
 	  break;
+	case REGISTRATION_REJECT:
+	  LOG_E(NAS, "Received Registration reject\n");
+	  exit(1);
+	  break;
           default:
             LOG_W(NR_RRC, "unknown message type %d\n", msg_type);
             break;
