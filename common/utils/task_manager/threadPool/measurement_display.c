f8257f985a65f688946f33561cd3b6821627902b
Cedric Roux <cedric.roux@eurecom.fr>
2024-08-22 14:08:28 +0200

    minor: no 'micro' character
    'git diff' was showing <C2><B5> instead of the 'micro' prefix symbol,
    so let's print 'microsecond' instead.

--- common/utils/task_manager/threadPool/measurement_display.c
+++ common/utils/task_manager/threadPool/measurement_display.c
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
   uint64_t deb = rdtsc_oai();
   usleep(100000);
   cpuCyclesMicroSec = (rdtsc_oai() - deb) / 100000;
-  printf("Cycles per µs: %lu\n", cpuCyclesMicroSec);
+  printf("Cycles per microsecond: %lu\n", cpuCyclesMicroSec);
   printf("Key" SEP "delay to process" SEP "processing time" SEP "delay to be read answer\n");
   notifiedFIFO_elt_t doneRequest;
 
