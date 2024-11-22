7edb74ae62d9d5fb041140fedece3615fa7755fa
Raymond Knopp <raymond.knopp@eurecom.fr>
2024-11-22 23:23:43 +0100

    debugging3

--- openair1/PHY/NR_TRANSPORT/nr_dlsch.c
+++ openair1/PHY/NR_TRANSPORT/nr_dlsch.c
@@ -220,7 +220,7 @@ void nr_generate_pdsch(processingData_L1tx_t *msgTx, int frame, int slot)
         }
         /// DMRS QPSK modulation
         NR_DL_FRAME_PARMS *fp = &gNB->frame_parms;
-#ifdef DLSCH_MAPPING
+#ifdef DEBUG_DLSCH_MAPPING
         printf("dlDmrsScramblingId %d, SCID %d slot %d l_symbol %d\n", rel15->dlDmrsScramblingId, rel15->SCID, slot, l_symbol);
 #endif
             const uint32_t *gold =
