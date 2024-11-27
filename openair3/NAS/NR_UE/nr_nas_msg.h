4da74dba913efcde3eb46573b34ce3e98e945bc5
Guido Casati <hello@guidocasati.com>
2024-11-27 14:46:49 +0100

    Add RRC Inactive Indication and Timer T3512 to NAS struct definition

--- openair3/NAS/NR_UE/nr_nas_msg.h
+++ openair3/NAS/NR_UE/nr_nas_msg.h
@@ -30,6 +30,7 @@
 #ifndef __NR_NAS_MSG_SIM_H__
 #define __NR_NAS_MSG_SIM_H__
 
+#include "common/utils/nr/nr_common.h"
 #include "RegistrationRequest.h"
 #include "FGSIdentityResponse.h"
 #include "FGSAuthenticationResponse.h"
@@ -104,6 +105,10 @@ typedef struct {
   uint8_t *registration_request_buf;
   uint32_t registration_request_len;
   instance_t UE_id;
+  /* RRC Inactive Indication */
+  bool is_rrc_inactive;
+  /* Timer T3512 */
+  NR_timer_t t3512;
 } nr_ue_nas_t;
 
 typedef enum fgs_protocol_discriminator_e {
