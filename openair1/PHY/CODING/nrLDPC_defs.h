40873d264dbef64f63b7ef49b1a3bc509b26485a
Raymond Knopp <raymond.knopp@eurecom.fr>
2025-01-21 21:15:39 +0100

    small fix after resolving conflicts during merge with develop

--- openair1/PHY/CODING/nrLDPC_defs.h
+++ openair1/PHY/CODING/nrLDPC_defs.h
@@ -58,15 +58,9 @@ typedef struct {
   time_stats_t *tprep;
   time_stats_t *tparity;
   time_stats_t *toutput;
-<<<<<<< HEAD
-  time_stats_t *dlsch_rate_matching_stats;
-  time_stats_t *dlsch_interleaving_stats;
-  int Kr;
-=======
   /// Size in bits of the code segments
   uint32_t K;
   /// Number of lifting sizes to fit the payload
->>>>>>> origin/develop
   uint32_t Kb;
   /// Lifting size
   uint32_t Zc;
