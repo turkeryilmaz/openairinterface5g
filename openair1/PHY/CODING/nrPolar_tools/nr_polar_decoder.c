c1d060c9efcc7efda10e43f719d7150a3326a3d9
beraoudabdelkhalek <ka_beraoud@esi.dz>
2025-02-10 11:58:18 +0100

    Remove new line from nr_polar_decoder.c and fix POLAR_CODING_DEBUG error

--- openair1/PHY/CODING/nrPolar_tools/nr_polar_decoder.c
+++ openair1/PHY/CODING/nrPolar_tools/nr_polar_decoder.c
@@ -835,24 +835,13 @@ uint32_t polar_decoder_int16(int16_t *input,
          Ar,
          B[1],
          B[0],
-         Cprime[1],
-         Cprime[0],
+        //  Cprime[1],
+        //  Cprime[0],
          crc,
          rxcrc,
          crc ^ rxcrc,
          polarParams->payloadBits);
 #endif
-
-  // printf("A %lx B %lx|%lx Cprime %lx|%lx (crc %x,rxcrc %lx, XOR %lx, bits%d)\n",
-  //        Ar,
-  //        B[1],
-  //        B[0],
-  //       //  Cprime[1],
-  //       //  Cprime[0],
-  //        crc,
-  //        rxcrc,
-  //        crc ^ rxcrc,
-  //        polarParams->payloadBits);
 
   out[0] = Ar;
 
