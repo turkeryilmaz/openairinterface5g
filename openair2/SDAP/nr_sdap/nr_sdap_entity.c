23d22d6c958acc4bca0de48299087ce2fff020ce
Guido Casati <hello@guidocasati.com>
2025-04-28 10:14:38 +0200

    Print errno in nr_sdap_rx_entity

--- openair2/SDAP/nr_sdap/nr_sdap_entity.c
+++ openair2/SDAP/nr_sdap/nr_sdap_entity.c
@@ -329,7 +329,7 @@ static void nr_sdap_rx_entity(nr_sdap_entity_t *entity,
     LOG_D(SDAP, "RX Entity offset : %d\n", offset);
 
     if (len != size-offset)
-      LOG_E(SDAP, "%s:%d:%s: fatal\n", __FILE__, __LINE__, __FUNCTION__);
+      LOG_E(SDAP, "%s: write failed! errno = %s\n", __func__, strerror(errno));
   }
 }
 
