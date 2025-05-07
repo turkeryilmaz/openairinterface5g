ba585cd96da44c4e2b9b3fed8b4588a678405685
Bartosz Podrygajlo <bartosz.podrygajlo@openairinterface.org>
2025-05-07 15:40:05 +0200

    Add deregistration for all UE instances.

--- executables/nr-uesoftmodem.c
+++ executables/nr-uesoftmodem.c
@@ -326,9 +326,11 @@ static void trigger_stop(int sig)
 static void trigger_deregistration(int sig)
 {
   if (!stop_immediately && IS_SA_MODE(get_softmodem_params())) {
-    MessageDef *msg = itti_alloc_new_message(TASK_NAS_NRUE, 0, NAS_DEREGISTRATION_REQ);
-    NAS_DEREGISTRATION_REQ(msg).cause = AS_DETACH;
-    itti_send_msg_to_task(TASK_NAS_NRUE, 0, msg);
+    for (int ue_inst = 0; ue_inst < NB_UE_INST; ue_inst++) {
+      MessageDef *msg = itti_alloc_new_message(TASK_NAS_NRUE, ue_inst, NAS_DEREGISTRATION_REQ);
+      NAS_DEREGISTRATION_REQ(msg).cause = AS_DETACH;
+      itti_send_msg_to_task(TASK_NAS_NRUE, ue_inst, msg);
+    }
     stop_immediately = true;
     static const char m[] = "Press ^C again to trigger immediate shutdown\n";
     __attribute__((unused)) int unused = write(STDOUT_FILENO, m, sizeof(m) - 1);
