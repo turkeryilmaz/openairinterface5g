d5e97ca6feacfbb1ec6b6bbd72cf4a00bf0f3ad4
Sakthivel Velumani <mail@sakthi.me>
2025-05-12 16:13:30 -0400

    Dlsim dump processing buffers
    Write extracted rxF buffer to output bin file.
    Fix offset in writing rxF buffer.

--- tools/plots/dlsim_ext_data.py
+++ tools/plots/dlsim_ext_data.py
@@ -5,23 +5,62 @@
 
 
 import numpy as np
+import matplotlib.pyplot as plt
+import struct
 import pandas as pd
 
 
-# In[98]:
+# In[109]:
 
 
-dataPath = './'
-csv_filename = 'iq_643_6_ueTimeDomainSamples_mcs23' + '.csv'
+dataPath = '/home/sakthi/work/dlsim_perf/'
+#csv_filename = 'iq_14_6_ueTimeDomainSamples_mcs26' + '.csv'
+csv_filename = 'iq_197_6_ueTimeDomainSamples_mcs26_powerDrop1dB' + '.csv'
 df = pd.read_csv(dataPath + csv_filename, sep=';')
 dataR = df['real'].to_numpy().astype(np.int16)
 dataI = df['imag'].to_numpy().astype(np.int16)
+#shift = 2200
+#dataR = np.roll(dataR, shift)
+#dataI = np.roll(dataI, shift)
+plt.plot(dataR)
+print(dataI)
 dataInter = np.zeros((2*dataR.size),dtype=np.int16)
 dataInter[0::2] = dataR
 dataInter[1::2] = dataI
+print(dataInter)
 
 
-# In[99]:
+# In[78]:
+
+
+slotLen = 30720
+numSlots = 10
+slot = 6
+dataFrame = np.zeros((numSlots * slotLen,2), dtype=np.int16)
+dataFrame[slotLen*6:slotLen*6+slotLen,0] = dataR
+dataFrame[slotLen*6:slotLen*6+slotLen,1] = dataI
+write_file = 'wavejudge_input.txt'
+np.savetxt(dataPath + write_file,dataFrame,delimiter=',', fmt='%d')
+
+
+# In[80]:
+
+
+# PDSCH config
+import yaml
+
+config_file = 'PDCCH_703-6_MCS22'
+config_ext = '.txt'
+with open(dataPath + config_file + config_ext, "r") as f:
+    content = f.read().replace('\t', '  ')  # two spaces
+
+with open(dataPath + config_file + 'space' + config_ext, "w") as f:
+    f.write(content)
+
+print(content)
+
+
+# In[107]:
 
 
 cell_config = {
@@ -33,7 +72,17 @@ cell_config = {
 }
 
 
-# In[101]:
+# In[100]:
+
+
+write_file = 'dlsim_input_data.bin'
+with open(dataPath + write_file, 'wb') as f:
+    for key,value in cell_config.items():
+        f.write(np.uint64(value).tobytes()) # write pdsch config
+    dataInter.tofile(f)            # Write time domain samples
+
+
+# In[110]:
 
 
 # Create bin file with sib1, rrcsetup config and samples
@@ -53,3 +102,16 @@ with open(dataPath + write_file, 'wb') as wf:
         wf.write(data)
     wf.write(np.uint64(dataInter.size))
     dataInter.tofile(wf)
+
+
+# In[50]:
+
+
+get_ipython().system('jupyter nbconvert --to script dlsim_ext_data.ipynb')
+
+
+# In[ ]:
+
+
+
+
