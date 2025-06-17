1a12ae3243e688202fa4879e02e2473ec1e73b42
Robert Schmidt <robert.schmidt@openairinterface.org>
2025-06-17 19:13:30 +0200

    Remove useless parameter for UE context modification response callback

--- openair2/LAYER2/NR_MAC_gNB/mac_rrc_ul_direct.c
+++ openair2/LAYER2/NR_MAC_gNB/mac_rrc_ul_direct.c
@@ -93,10 +93,8 @@ static void ue_context_setup_response_direct(const f1ap_ue_context_setup_t *resp
   itti_send_msg_to_task(TASK_RRC_GNB, 0, msg);
 }
 
-static void ue_context_modification_response_direct(const f1ap_ue_context_modif_req_t *req,
-                                                    const f1ap_ue_context_modif_resp_t *resp)
+static void ue_context_modification_response_direct(const f1ap_ue_context_modif_resp_t *resp)
 {
-  (void)req; /* we don't need the request -- it is to set up GTP in F1 case */
   MessageDef *msg = itti_alloc_new_message(TASK_MAC_GNB, 0, F1AP_UE_CONTEXT_MODIFICATION_RESP);
   msg->ittiMsgHeader.originInstance = -1; // means monolithic
   f1ap_ue_context_modif_resp_t *f1ap_msg = &F1AP_UE_CONTEXT_MODIFICATION_RESP(msg);
