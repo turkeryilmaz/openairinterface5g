080a90c930743446c37a21ad4db9e50943f898db
Cedric Roux <cedric.roux@eurecom.fr>
2025-06-26 13:02:56 +0200

    bugfix: UE AMBR is optional
    This was a problem with open5gs and more than one PDU session.
    The core network sets UE AMBR only once, leading to failure in the gNB
    for the second PDU session.
    
    (A proper handling of AMBR has to be develop at some point in the gNB.)

--- openair2/RRC/NR/rrc_gNB_NGAP.c
+++ openair2/RRC/NR/rrc_gNB_NGAP.c
@@ -841,7 +841,9 @@ void rrc_gNB_process_NGAP_PDUSESSION_SETUP_REQ(MessageDef *msg_p, instance_t ins
   for (int i = 0; i < msg->nb_pdusessions_tosetup; ++i)
     cp_pdusession_resource_item_to_pdusession(&to_setup[i], &msg->pdusession[i]);
 
-  if (!trigger_bearer_setup(rrc, UE, msg->nb_pdusessions_tosetup, to_setup, msg->ueAggMaxBitRate.br_dl)) {
+  uint64_t dl_ambr = msg->has_ue_ambr ? msg->ueAggMaxBitRate.br_dl : 0;
+
+  if (!trigger_bearer_setup(rrc, UE, msg->nb_pdusessions_tosetup, to_setup, dl_ambr)) {
     // Reject PDU Session Resource setup if there's no CU-UP associated
     LOG_W(NR_RRC, "UE %d: reject PDU Session Setup in PDU Session Resource Setup Response\n", UE->rrc_ue_id);
     ngap_cause_t cause = {.type = NGAP_CAUSE_RADIO_NETWORK, .value = NGAP_CAUSE_RADIO_NETWORK_RESOURCES_NOT_AVAILABLE_FOR_THE_SLICE};
