e4876d4c0c4148d050061306b804a22fbd4f09ee
Jaroslava Fiedlerova <Jaroslava.Fiedlerova@openairinterface.org>
2025-08-01 14:28:45 +0200

    CI: make generic function for OC chart deployments

--- ci-scripts/scripts/oc-chart-deploy.sh
+++ ci-scripts/scripts/oc-chart-deploy.sh
@@ -8,8 +8,9 @@ function die() { echo $@; exit 1; }
 OC_DIR=${1}
 OC_NS=${2}
 IMAGE_TAG=${3}
+OC_RELEASE=$(basename "${OC_DIR}")
 
 cat ${OC_DIR}/oc-password | oc login -u oaicicd --server https://api.oai.cs.eurecom.fr:6443 > /dev/null
 oc project ${OC_NS} > /dev/null
-helm install --wait oai-cu ${OC_DIR}/ci-scripts/charts/oai-cu/.
+helm install --wait --timeout 120s ${OC_RELEASE} --set nfimage.version=${IMAGE_TAG} ${OC_DIR}/.
 oc logout > /dev/null
