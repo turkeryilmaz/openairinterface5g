d702bcd657370fe2de01c7529084d2c968f7e586
Romain Beurdouche <romain.beurdouche@eurecom.fr>
2025-08-07 09:06:51 +0000

    fix(LDPCdecoder): Cleanup segment decoder interface
    Variables 'harq_pid', 'ulsch_id' and 'C' of the segment decoder
    interface were not used any more by the implementations and set to zero
    by all the callers.
    Then it is more relevant and compliant to remove them.

--- openair1/PHY/CODING/nrLDPC_decoder_LYC/nrLDPC_decoder_LYC.cu
+++ openair1/PHY/CODING/nrLDPC_decoder_LYC/nrLDPC_decoder_LYC.cu
@@ -506,9 +506,6 @@ extern "C" void LDPCshutdown()
 }
 
 extern "C"  int32_t LDPCdecoder(t_nrLDPC_dec_params *p_decParams,
-                                    uint8_t harq_pid,
-                                    uint8_t ulsch_id,
-                                    uint8_t C,
                                     int8_t *p_llr,
                                     int8_t *p_out,
                                     t_nrLDPC_time_stats *,
