agamigoind/OAI_5G

rework SRB0 handling in RLC

executables/nr-ue.c
@@ -1227,7 +1227,7 @@ void init_NR_UE(int nb_inst, char *uecap_file, char *reconfig_file, char *rbconf
     AssertFatal(mac->if_module, "can not initialize IF module\n");
     if (!IS_SA_MODE(get_softmodem_params()) && !get_softmodem_params()->sl_mode) {
       init_nsa_message(&rrc_inst[i], reconfig_file, rbconfig_file);
-      nr_rlc_activate_srb0(mac_inst[i].crnti, NULL, send_srb0_rrc);
+      nr_rlc_init_ue(i);
     }
     //TODO: Move this call to RRC
     start_sidelink((&rrc_inst[i])->ue_id);

openair2/LAYER2/NR_MAC_UE/main_ue_nr.c
@@ -40,17 +40,6 @@
 #include <pthread.h>
 static NR_UE_MAC_INST_t *nr_ue_mac_inst; 
 
-void send_srb0_rrc(int ue_id, const uint8_t *sdu, sdu_size_t sdu_len, void *data)
-{
-  AssertFatal(sdu_len > 0 && sdu_len < CCCH_SDU_SIZE, "invalid CCCH SDU size %d\n", sdu_len);
-
-  MessageDef *message_p = itti_alloc_new_message(TASK_MAC_UE, 0, NR_RRC_MAC_CCCH_DATA_IND);
-  memset(NR_RRC_MAC_CCCH_DATA_IND(message_p).sdu, 0, sdu_len);
-  memcpy(NR_RRC_MAC_CCCH_DATA_IND(message_p).sdu, sdu, sdu_len);
-  NR_RRC_MAC_CCCH_DATA_IND(message_p).sdu_size = sdu_len;
-  itti_send_msg_to_task(TASK_RRC_NRUE, ue_id, message_p);
-}
-
 void nr_ue_init_mac(NR_UE_MAC_INST_t *mac)
 {
   LOG_I(NR_MAC, "[UE%d] Initializing MAC\n", mac->ue_id);
@@ -144,9 +133,8 @@ NR_UE_MAC_INST_t *nr_l2_init_ue(int nb_inst)
   int rc = nr_rlc_module_init(0);
   AssertFatal(rc == 0, "Could not initialize RLC layer\n");
 
-  for (int j = 0; j < nb_inst; j++) {
-    nr_rlc_activate_srb0(j, NULL, send_srb0_rrc);
-  }
+  for (int j = 0; j < nb_inst; j++)
+    nr_rlc_init_ue(j);
 
   return (nr_ue_mac_inst);
 }

turkeryilmaz/openairinterface5g

executables/nr-ue.c
@@ -1293,7 +1293,7 @@ void init_NR_UE(int nb_inst, char *uecap_file, char *reconfig_file, char *rbconf
     AssertFatal(mac->if_module, "can not initialize IF module\n");
     if (!IS_SA_MODE(get_softmodem_params()) && !get_softmodem_params()->sl_mode) {
       init_nsa_message(rrc, reconfig_file, rbconfig_file);
-      nr_rlc_activate_srb0(mac->crnti, NULL, send_srb0_rrc);
+      nr_rlc_init_ue(instance_id);
     }
     //TODO: Move this call to RRC
     start_sidelink(instance_id);

openair2/LAYER2/NR_MAC_UE/main_ue_nr.c
@@ -40,7 +40,7 @@
 #include <pthread.h>
 static NR_UE_MAC_INST_t *nr_ue_mac_inst[MAX_NUM_NR_UE_INST] = {0};
 
-void send_srb0_rrc(int ue_id, const uint8_t *sdu, sdu_size_t sdu_len, void *data)
+void send_initial_srb0_message(int ue_id, const uint8_t *sdu, sdu_size_t sdu_len)
 {
   AssertFatal(sdu_len > 0 && sdu_len < CCCH_SDU_SIZE, "invalid CCCH SDU size %d\n", sdu_len);
 
@@ -147,8 +147,7 @@ NR_UE_MAC_INST_t *nr_l2_init_ue(int instance_id)
     initialized = true;
   }
 
-  nr_rlc_activate_srb0(instance_id, NULL, send_srb0_rrc);
-
+  nr_rlc_init_ue(instance_id);
   return nr_ue_mac_inst[instance_id];
 }
 