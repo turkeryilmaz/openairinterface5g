ba42307fdc2069170461719297bd80b1438da1c6
Jaroslava Fiedlerova <Jaroslava.Fiedlerova@openairinterface.org>
2025-09-02 23:05:46 +0200

    Workaround: fix Msg3 allocation when mixed slot has < 3 UL symbols
    This patch corrects a regression introduced in !3521 where Msg3 could not be
    scheduled if a mixed slot had fewer than 3 UL symbols. Msg3 is now forced to
    use a full UL allocation if there are less than 3 UL symbols.
    This avoids assertion:
    
    Assertion (mcsindex <= 28) failed!
    In nr_add_msg3() /home/oaicicd/jaroslava/test_dir/2025.w33/openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c:1152
    Exceeding MCS limit for Msg3
    
    Note: This is a temporary fix and should be revisited when the Msg3 allocation
    logic for mixed slots is clarified.

--- openair2/LAYER2/NR_MAC_gNB/nr_radio_config.c
+++ openair2/LAYER2/NR_MAC_gNB/nr_radio_config.c
@@ -1115,7 +1115,7 @@ void nr_rrc_config_ul_tda(NR_ServingCellConfigCommon_t *scc, int min_fb_delay, i
 
     int N_dl1 = p1->nrofDownlinkSlots;
     int N_ul1 = p1->nrofUplinkSlots;
-    int N_dl2 = p2 ? p2->nrofDownlinkSlots : 0;
+    //int N_dl2 = p2 ? p2->nrofDownlinkSlots : 0;
     int N_ul2 = p2 ? p2->nrofUplinkSlots : 0;
     int tdd_period_idx = get_tdd_period_idx(scc->tdd_UL_DL_ConfigurationCommon);
     int nb_periods_per_frame = get_nb_periods_per_frame(tdd_period_idx);
@@ -1123,20 +1123,31 @@ void nr_rrc_config_ul_tda(NR_ServingCellConfigCommon_t *scc, int min_fb_delay, i
 
     // make TDA for the mixed slot
     long mixed_sliv = get_SLIV(NR_NUMBER_OF_SYMBOLS_PER_SLOT - ul_symb, ul_symb - 1);
-    if (ul_symb > 1 && (k2 <= N_dl1 || (p2 && N_ul2 == 0))) {
+    if (ul_symb > 2 && (k2 <= N_dl1 || (p2 && N_ul2 == 0))) {
       // UL TDA index 2 for mixed slot (TDD) (no pattern 2 and enough DL slots, or pattern2 only DL)
       tda = set_TimeDomainResourceAllocation(k2, mixed_sliv); // to be reached from suitable DL slot
       asn1cSeqAdd(&tda_list->list, tda);
-    } else if (ul_symb > 1 && !p2 && k2 > N_dl1) { // k2 > N_Dl1
+    } else if (ul_symb > 2 && !p2 && k2 > N_dl1) { // k2 > N_Dl1
       // UL TDA for mixed slot without pattern 2 and less DL slots than k2
       tda = set_TimeDomainResourceAllocation(nb_slots_per_period, mixed_sliv); // to be reached from last DL (mixed) slot, if any
       asn1cSeqAdd(&tda_list->list, tda);
-    } else if (ul_symb > 1 && N_ul2 > 0 && k2 > N_dl1) {
+    } else if (ul_symb > 2 && N_ul2 > 0 && k2 > N_dl1) {
       // we have pattern 2 with UL, and less p1 DL slots than k2
       tda = set_TimeDomainResourceAllocation(N_dl1 + N_ul2 + 1, mixed_sliv);
       asn1cSeqAdd(&tda_list->list, tda);
     } else {
-      AssertFatal(ul_symb == 0, "unhandled case of mixed slot: N_dl1 %d N_ul1 %d N_dl2 %d N_ul2 %d\n", N_dl1, N_ul1, N_dl2, N_ul2);
+      //AssertFatal(ul_symb == 0, "unhandled case of mixed slot: N_dl1 %d N_ul1 %d N_dl2 %d N_ul2 %d\n", N_dl1, N_ul1, N_dl2, N_ul2);
+    }
+
+    int k2_msg3 = nb_slots_per_period - get_delta_for_k2(mu);
+    int no_mix_slot = ul_symb < 3 ? 1 : 0; // we need at least 2 symbols for scheduling Msg3
+    k2_msg3 += no_mix_slot;
+    if (no_mix_slot) {
+      tda = set_TimeDomainResourceAllocation(k2_msg3, get_SLIV(0, 13)); // full allocation if there is no mixed slot
+      asn1cSeqAdd(&tda_list->list, tda);
+    } else {
+      tda = set_TimeDomainResourceAllocation(k2_msg3, mixed_sliv);
+      asn1cSeqAdd(&tda_list->list, tda);
     }
 
     // make TDA for UL slots that are not reachable within k2/min_rxtxtime
