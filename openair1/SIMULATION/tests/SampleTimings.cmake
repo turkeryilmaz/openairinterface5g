70d99b201ef3c6f1d634a611cafe04718e529338
Robert Schmidt <robert.schmidt@openairinterface.org>
2025-09-08 14:52:14 +0200

    Example timed phytest

---
+++ openair1/SIMULATION/tests/SampleTimings.cmake
@@ -0,0 +1,3 @@
+check_physim_threshold(robert "DLSCH LDPC output generation time" "< 5")
+check_physim_threshold(robert "PHY proc tx" "<= 90")
+check_physim_threshold(robert "DOES NOT EXIST" "<= 90")
