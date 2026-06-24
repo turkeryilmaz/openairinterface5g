d59cd8562ed8e565c47bd72efe38d623430c99b4
Teodora Vladić <teodora.vladic@openairinterface.org>
2026-06-24 14:09:04 +0200

    Remove `fh_south_asynch_in` as not used in NR
    Signed-off-by: Teodora Vladić <teodora.vladic@openairinterface.org>

--- executables/nr-ru.c
+++ executables/nr-ru.c
@@ -910,7 +910,6 @@ void set_function_spec_param(RU_t *ru)
       ru->feptx_ofdm = nr_feptx_tp; // need to do transmit Precoding + IDFTs
       ru->fh_south_in = fh_if5_south_in; // synchronous IF5 reception
       ru->fh_south_out = fh_if5_south_out; // synchronous IF5 transmission
-      ru->fh_south_asynch_in = NULL; // no asynchronous UL
       ru->start_rf = ru->eth_params.transp_preference == ETH_UDP_IF5_ECPRI_MODE ? start_streaming : NULL;
       ru->stop_rf = NULL;
       ru->start_write_thread = NULL;
@@ -925,7 +924,6 @@ void set_function_spec_param(RU_t *ru)
       ru->feptx_ofdm = NULL; // no OFDM mod
       ru->fh_south_in = NULL;
       ru->fh_south_out = NULL;
-      ru->fh_south_asynch_in = NULL;
       ru->fh_north_asynch_in = NULL;
       ru->start_rf = NULL; // no local RF
       ru->stop_rf = NULL;
