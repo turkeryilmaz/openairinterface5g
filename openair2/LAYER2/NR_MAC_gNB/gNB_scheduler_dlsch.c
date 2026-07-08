5e6dd5d4e862eb4a2512cc82796d0d4ceb402489
Robert Schmidt <robert.schmidt@openairinterface.org>
2026-07-08 15:37:35 +0200

    Schedule multiple UEs in one DL slot
    Schedule multiple UEs in a single DL slot through two passes over
    eligible UEs. In the first pass, for N+2 UEs, check the amount of RBs
    necessary, where N is the maximum number of UEs to be allocated. Check
    +2 because some UEs might not be able to be allocated (e.g., CCE
    conflict), so we might need additional UEs. For the first N UEs, also store
    if there are additional RBs that are not used by equal sharing (not for
    +2, as this would lead to not using all excess RBs). In the second pass,
    allocate the PRBs, taking into account excess RBs that are given in a
    first-come-first-serve order to hold the PF property.
    
    Two passes are necessary:
    
    - we don't know how many PRBs each UE will require, so we need to start
      conservative on the first UE(s)
    - if later UEs don't require all PRBs, we maintain PF by given more to
      the high priority UE. If we give more to a low prio UE, we might have
      pathetic cases in which a UE is second UE, gets more resources, which
      in turn keeps it in second position etc.
    
    Signed-off-by: Robert Schmidt <robert.schmidt@openairinterface.org>

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_dlsch.c
@@ -886,11 +886,9 @@ void nr_dlsch_preprocessor(gNB_MAC_INST *mac, post_process_pdsch_t *pp_pdsch)
   for (int i = 0; i < num_beams; i++)
     n_rb_sched[i] = bw;
 
-  int average_agg_level = 4; // TODO find a better estimation
-  int max_sched_ues = bw / (average_agg_level * NR_NB_REG_PER_CCE);
-
   // FAPI cannot handle more than MAX_DCI_CORESET DCIs
-  max_sched_ues = min(max_sched_ues, MAX_DCI_CORESET);
+  static_assert(4 < MAX_DCI_CORESET, "cannot have more concurrent UEs than MAX_DCI_CORESET\n");
+  int max_sched_ues = 4;
 
   nr_dl_schedule(mac, pp_pdsch, UE_info->connected_ue_list, max_sched_ues, num_beams, n_rb_sched);
 }
