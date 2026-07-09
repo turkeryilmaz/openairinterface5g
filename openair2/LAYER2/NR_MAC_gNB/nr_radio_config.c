14d83cf7cb7c61c0021ec00ea8a6187a63fb09b4
Francesco Mani <email@francescomani.it>
2026-07-09 16:21:02 +0200

    change name of resource set counter to avoid ambiguity
    Signed-off-by: Francesco Mani <email@francescomani.it>

--- openair2/LAYER2/NR_MAC_gNB/nr_radio_config.c
+++ openair2/LAYER2/NR_MAC_gNB/nr_radio_config.c
@@ -894,9 +894,9 @@ static NR_SetupRelease_SRS_Config_t *get_config_srs(const NR_ServingCellConfigCo
 
   srs_Config->srs_ResourceSetToAddModList = calloc_or_fail(1, sizeof(*srs_Config->srs_ResourceSetToAddModList));
   int k2 = minRXTXTIME;
-  int num_reset = 1;
+  int num_res_set = 1;
   const long usage = NR_SRS_ResourceSet__usage_codebook;
-  NR_SRS_ResourceSet_t *srs_resset = get_srs_resourceset(num_reset, res_id, usage, k2, num_reset, do_srs);
+  NR_SRS_ResourceSet_t *srs_resset = get_srs_resourceset(num_res_set, res_id, usage, k2, num_res_set, do_srs);
   asn1cSeqAdd(&srs_Config->srs_ResourceSetToAddModList->list, srs_resset);
   if (do_srs == APERIODIC_SRS) {
     NR_PUSCH_TimeDomainResourceAllocationList_t *tda_list =
@@ -904,12 +904,12 @@ static NR_SetupRelease_SRS_Config_t *get_config_srs(const NR_ServingCellConfigCo
     for (int i = 0; i < tda_list->list.count; ++i) {
       if (k2 == *tda_list->list.array[i]->k2)
         continue;
-      num_reset++;
+      num_res_set++;
       k2 = *tda_list->list.array[i]->k2;
       // in case of UL heavy configuration better use periodic SRS if there are more UL slots than allowed trigger states
-      AssertFatal(num_reset < 4, "Exceeded the number of allowed SRS trigger states.\n");
+      AssertFatal(num_res_set < 4, "Exceeded the number of allowed SRS trigger states.\n");
       const long usage = NR_SRS_ResourceSet__usage_codebook;
-      NR_SRS_ResourceSet_t *srs_resset = get_srs_resourceset(num_reset, res_id, usage, k2, num_reset, do_srs);
+      NR_SRS_ResourceSet_t *srs_resset = get_srs_resourceset(num_res_set, res_id, usage, k2, num_res_set, do_srs);
       asn1cSeqAdd(&srs_Config->srs_ResourceSetToAddModList->list, srs_resset);
     }
   }
