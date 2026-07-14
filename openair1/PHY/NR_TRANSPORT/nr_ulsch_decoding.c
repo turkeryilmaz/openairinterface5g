HarshaUndodi/Handover-simulation-networking-

commit 28cff29178dd19aa5016571d25fbee2d5f6655dd

Simplify TB_parameters, c, d, d_to_be_cleared
The upcoming LDPC CUDA offload library needs a contiguous buffer instead
of individual segments. Correspondingly, change the interface to
accommodate this need, in the CPU slot decoding library and AAL.

Co-authored-by: Robert Schmidt <robert.schmidt@openairinterface.org>
Co-authored-by: Jaroslava Fiedlerova <jaroslava.fiedlerova@openairinterface.org>
Co-authored-by: Romain Beurdouche <romain.beurdouche@eurecom.fr>
Signed-off-by: Robert Schmidt <robert.schmidt@openairinterface.org>

openair1/PHY/NR_TRANSPORT/nr_ulsch_decoding.c
@@ -47,10 +47,6 @@ void free_gNB_ulsch(NR_gNB_ULSCH_t *ulsch, uint16_t N_RB_UL)
       free_and_zero(ulsch->harq_process->b);
       ulsch->harq_process->b = NULL;
     }
-    for (int r = 0; r < a_segments; r++) {
-      free_and_zero(ulsch->harq_process->c[r]);
-      free_and_zero(ulsch->harq_process->d[r]);
-    }
     free_and_zero(ulsch->harq_process->c);
     free_and_zero(ulsch->harq_process->d);
     free_and_zero(ulsch->harq_process->d_to_be_cleared);
@@ -79,12 +75,9 @@ NR_gNB_ULSCH_t new_gNB_ulsch(uint8_t max_ldpc_iterations, uint16_t N_RB_UL)
   init_abort(&harq->abort_decode);
   ulsch.harq_process = harq;
   harq->b = malloc16_clear(ulsch_bytes * sizeof(*harq->b));
-  harq->c = malloc16_clear(a_segments * sizeof(*harq->c));
-  harq->d = malloc16_clear(a_segments * sizeof(*harq->d));
-  for (int r = 0; r < a_segments; r++) {
-    harq->c[r] = malloc16_clear(8448 * sizeof(*harq->c[r]));
-    harq->d[r] = malloc16_clear(68 * 384 * sizeof(*harq->d[r]));
-  }
+  // Allocate one contiguous buffer fr all c/d arrays to simplify addressing for GPU LDPC offload
+  harq->c = malloc16_clear(a_segments * 8448 * sizeof(*harq->c));
+  harq->d = malloc16_clear(a_segments * 68 * 384 * sizeof(*harq->d));
   harq->d_to_be_cleared = calloc(a_segments, sizeof(bool));
   AssertFatal(harq->d_to_be_cleared != NULL, "out of memory\n");
   return (ulsch);
@@ -223,9 +216,6 @@ int nr_ulsch_decoding(PHY_VARS_gNB *phy_vars_gNB,
     set_abort(&harq_process->abort_decode, false);
   }
 
-  nrLDPC_segment_decoding_parameters_t segments[nb_pusch][max_num_segments];
-  memset(segments, 0, sizeof(segments));
-
   for (uint8_t pusch_id = 0; pusch_id < nb_pusch; pusch_id++) {
     uint8_t ULSCH_id = ULSCH_ids[pusch_id];
     NR_gNB_ULSCH_t *ulsch = &phy_vars_gNB->ulsch[ULSCH_id];
@@ -238,66 +228,79 @@ int nr_ulsch_decoding(PHY_VARS_gNB *phy_vars_gNB,
     }
 
     nrLDPC_TB_decoding_parameters_t *TB_parameters = &TBs[pusch_id];
-    TB_parameters->segments = segments[pusch_id];
 
-    uint32_t r_offset = 0;
+    TB_parameters->llr = ulsch_llr;
+    TB_parameters->d = harq_process->d;
+    TB_parameters->c = harq_process->c;
+    TB_parameters->E = nr_get_E(TB_parameters->G, TB_parameters->C, TB_parameters->Qm, TB_parameters->nb_layers, 0);
+    TB_parameters->first_rE2 = TB_parameters->C;
+    TB_parameters->E2 = TB_parameters->E;
+    TB_parameters->R = nr_get_R_ldpc_decoder(TB_parameters->rv_index,
+                                             TB_parameters->E,
+                                             TB_parameters->BG,
+                                             TB_parameters->Z,
+                                             &harq_process->llrLen,
+                                             harq_process->round);
+    for (int r = 0; r < TB_parameters->C; r++)
+      TB_parameters->decodeSuccess[r] = false;
+    TB_parameters->d_to_be_cleared = harq_process->harq_to_be_cleared;
+    reset_meas(&TB_parameters->ts_deinterleave);
+    reset_meas(&TB_parameters->ts_rate_unmatch);
+    reset_meas(&TB_parameters->ts_seg_prep);
+    reset_meas(&TB_parameters->ts_ldpc_decode);
     for (int r = 0; r < TB_parameters->C; r++) {
-      nrLDPC_segment_decoding_parameters_t *segment_parameters = &TB_parameters->segments[r];
-      segment_parameters->E = nr_get_E(TB_parameters->G, TB_parameters->C, TB_parameters->Qm, TB_parameters->nb_layers, r);
-      segment_parameters->R = nr_get_R_ldpc_decoder(TB_parameters->rv_index,
-                                                    segment_parameters->E,
-                                                    TB_parameters->BG,
-                                                    TB_parameters->Z,
-                                                    &harq_process->llrLen,
-                                                    harq_process->round);
-      segment_parameters->llr = ulsch_llr + r_offset;
-      segment_parameters->d = harq_process->d[r];
-      segment_parameters->d_to_be_cleared = &harq_process->d_to_be_cleared[r];
-      segment_parameters->c = harq_process->c[r];
-      segment_parameters->decodeSuccess = false;
-
-      reset_meas(&segment_parameters->ts_deinterleave);
-      reset_meas(&segment_parameters->ts_rate_unmatch);
-      reset_meas(&segment_parameters->ts_ldpc_decode);
-
-      r_offset += segment_parameters->E;
-    }
-    if (harq_process->harq_to_be_cleared) {
-      for (int r = 0; r < TB_parameters->C; r++) {
-        harq_process->d_to_be_cleared[r] = true;
+      int Etmp = nr_get_E(TB_parameters->G, TB_parameters->C, TB_parameters->Qm, TB_parameters->nb_layers, r);
+      if (TB_parameters->E != Etmp) {
+        TB_parameters->E2 = Etmp;
+        TB_parameters->R2 = nr_get_R_ldpc_decoder(TB_parameters->rv_index,
+                                                  TB_parameters->E2,
+                                                  TB_parameters->BG,
+                                                  TB_parameters->Z,
+                                                  &harq_process->llrLen,
+                                                  harq_process->round);
+        TB_parameters->first_rE2 = r;
+        break;
       }
-      harq_process->harq_to_be_cleared = false;
     }
   }
 
   int ret_decoder = phy_vars_gNB->nrLDPC_coding_interface.nrLDPC_coding_decoder(&slot_parameters);
-
   // post decode
   for (uint8_t pusch_id = 0; pusch_id < nb_pusch; pusch_id++) {
     uint8_t ULSCH_id = ULSCH_ids[pusch_id];
     NR_gNB_ULSCH_t *ulsch = &phy_vars_gNB->ulsch[ULSCH_id];
     NR_UL_gNB_HARQ_t *harq_process = ulsch->harq_process;
 
-    nrLDPC_TB_decoding_parameters_t TB_parameters = TBs[pusch_id];
+    nrLDPC_TB_decoding_parameters_t *TB_parameters = &TBs[pusch_id];
 
-    uint32_t offset = 0;
-    for (int r = 0; r < TB_parameters.C; r++) {
-      nrLDPC_segment_decoding_parameters_t nrLDPC_segment_decoding_parameters = TB_parameters.segments[r];
-      // Copy c to b in case of decoding success
-      if (nrLDPC_segment_decoding_parameters.decodeSuccess) {
-        memcpy(harq_process->b + offset,
-               harq_process->c[r],
-               (harq_process->K >> 3) - (harq_process->F >> 3) - ((harq_process->C > 1) ? 3 : 0));
-      } else {
-        LOG_D(PHY, "uplink segment error %d/%d\n", r, harq_process->C);
-        LOG_D(PHY, "ULSCH %d in error\n", ULSCH_id);
+    uint32_t offset = 0, r_offset = 0;
+    bool crcok = true;
+    LOG_D(PHY, "C = %d\n", TB_parameters->C);
+    for (int r = 0; r < TB_parameters->C; r++) {
+      LOG_D(PHY, "Segment %d %d\n", r, TB_parameters->decodeSuccess[r]);
+      if (TB_parameters->decodeSuccess[r] == false) {
+        LOG_D(PHY, "Segment %d/%d in error\n", r, TB_parameters->C);
+        crcok = false;
+        break;
       }
-      offset += ((harq_process->K >> 3) - (harq_process->F >> 3) - ((harq_process->C > 1) ? 3 : 0));
-
-      merge_meas(&phy_vars_gNB->ts_deinterleave, &nrLDPC_segment_decoding_parameters.ts_deinterleave);
-      merge_meas(&phy_vars_gNB->ts_rate_unmatch, &nrLDPC_segment_decoding_parameters.ts_rate_unmatch);
-      merge_meas(&phy_vars_gNB->ts_ldpc_decode, &nrLDPC_segment_decoding_parameters.ts_ldpc_decode);
     }
+    if (crcok) {
+      for (int r = 0; r < TB_parameters->C; r++) {
+        // Copy c to b in case of decoding success
+        memcpy(harq_process->b + offset,
+               harq_process->c + r_offset,
+               (harq_process->K >> 3) - (harq_process->F >> 3) - ((harq_process->C > 1) ? 3 : 0));
+        offset += ((harq_process->K >> 3) - (harq_process->F >> 3) - ((harq_process->C > 1) ? 3 : 0));
+        r_offset += (harq_process->K >> 3);
+      }
+    } else {
+      LOG_D(PHY, "ULSCH %d in error\n", ULSCH_id);
+    }
+    merge_meas(&phy_vars_gNB->ts_deinterleave, &TB_parameters->ts_deinterleave);
+    merge_meas(&phy_vars_gNB->ts_rate_unmatch, &TB_parameters->ts_rate_unmatch);
+    merge_meas(&phy_vars_gNB->ts_seg_prep, &TB_parameters->ts_seg_prep);
+    merge_meas(&phy_vars_gNB->ts_ldpc_decode, &TB_parameters->ts_ldpc_decode);
+    harq_process->harq_to_be_cleared = false;
   }
 
   return ret_decoder;

openair1/PHY/CODING/nrLDPC_coding/nrLDPC_coding_aal/nrLDPC_coding_aal.c
@@ -458,8 +458,9 @@ static int init_op_data_objs_dec(struct rte_bbdev_op_data *bufs,
   bool large_input = false;
   int j = 0;
   for (int h = 0; h < nrLDPC_slot_decoding_parameters->nb_TBs; ++h) {
-    for (int i = 0; i < nrLDPC_slot_decoding_parameters->TBs[h].C; ++i) {
-      uint32_t data_len = nrLDPC_slot_decoding_parameters->TBs[h].segments[i].E;
+    nrLDPC_TB_decoding_parameters_t *p = &nrLDPC_slot_decoding_parameters->TBs[h];
+    for (int i = 0; i < p->C; ++i) {
+      uint32_t data_len = i < p->first_rE2 ? p->E : p->E2;
       char *data;
       struct rte_mbuf *m_head = rte_pktmbuf_alloc(mbuf_pool);
       AssertFatal(m_head != NULL,
@@ -602,20 +603,19 @@ static void set_ldpc_dec_op(struct rte_bbdev_dec_op **ops,
   bool special_case_tb_mode = !active_dev.is_t2 && (nrLDPC_slot_decoding_parameters->nb_TBs == 1)
                               && (nb_segments_decoding(nrLDPC_slot_decoding_parameters) == 1);
   for (int h = 0; h < nrLDPC_slot_decoding_parameters->nb_TBs; ++h) {
-    for (int i = 0; i < nrLDPC_slot_decoding_parameters->TBs[h].C; ++i) {
-      ops[j]->ldpc_dec.basegraph = nrLDPC_slot_decoding_parameters->TBs[h].BG;
-      ops[j]->ldpc_dec.z_c = nrLDPC_slot_decoding_parameters->TBs[h].Z;
-      ops[j]->ldpc_dec.q_m = nrLDPC_slot_decoding_parameters->TBs[h].Qm;
-      ops[j]->ldpc_dec.n_filler = nrLDPC_slot_decoding_parameters->TBs[h].F;
-      ops[j]->ldpc_dec.n_cb = (nrLDPC_slot_decoding_parameters->TBs[h].BG == 1) ? (66 * nrLDPC_slot_decoding_parameters->TBs[h].Z)
-                                                                                : (50 * nrLDPC_slot_decoding_parameters->TBs[h].Z);
-      ops[j]->ldpc_dec.iter_max = nrLDPC_slot_decoding_parameters->TBs[h].max_ldpc_iterations;
-      ops[j]->ldpc_dec.rv_index = nrLDPC_slot_decoding_parameters->TBs[h].rv_index;
+    nrLDPC_TB_decoding_parameters_t *p = &nrLDPC_slot_decoding_parameters->TBs[h];
+    for (int i = 0; i < p->C; ++i) {
+      ops[j]->ldpc_dec.basegraph = p->BG;
+      ops[j]->ldpc_dec.z_c = p->Z;
+      ops[j]->ldpc_dec.q_m = p->Qm;
+      ops[j]->ldpc_dec.n_filler = p->F;
+      ops[j]->ldpc_dec.n_cb = (p->BG == 1) ? (66 * p->Z) : (50 * p->Z);
+      ops[j]->ldpc_dec.iter_max = p->max_ldpc_iterations;
+      ops[j]->ldpc_dec.rv_index = p->rv_index;
       ops[j]->ldpc_dec.op_flags = RTE_BBDEV_LDPC_ITERATION_STOP_ENABLE | RTE_BBDEV_LDPC_HQ_COMBINE_OUT_ENABLE;
-      if (*nrLDPC_slot_decoding_parameters->TBs[h].segments[i].d_to_be_cleared) {
-        *nrLDPC_slot_decoding_parameters->TBs[h].segments[i].d_to_be_cleared = false;
+      if (p->d_to_be_cleared) {
         if (active_dev.is_t2)
-          *nrLDPC_slot_decoding_parameters->TBs[h].processedSegments = 0;
+          *p->processedSegments = 0;
       } else {
         ops[j]->ldpc_dec.op_flags |= RTE_BBDEV_LDPC_HQ_COMBINE_IN_ENABLE;
         if (active_dev.support_internal_harq_memory) {
@@ -625,12 +625,12 @@ static void set_ldpc_dec_op(struct rte_bbdev_dec_op **ops,
       }
 
       if (!active_dev.is_t2)
-        *nrLDPC_slot_decoding_parameters->TBs[h].processedSegments = 0;
+        *p->processedSegments = 0;
 
       if (!special_case_tb_mode) {
         ops[j]->ldpc_dec.code_block_mode = 1;
-        ops[j]->ldpc_dec.cb_params.e = nrLDPC_slot_decoding_parameters->TBs[h].segments[i].E;
-        if (nrLDPC_slot_decoding_parameters->TBs[h].C > 1) {
+        ops[j]->ldpc_dec.cb_params.e = i < p->first_rE2 ? p->E : p->E2;
+        if (p->C > 1) {
           ops[j]->ldpc_dec.op_flags |= RTE_BBDEV_LDPC_CRC_TYPE_24B_DROP;
           ops[j]->ldpc_dec.op_flags |= RTE_BBDEV_LDPC_CRC_TYPE_24B_CHECK;
         }
@@ -646,12 +646,12 @@ static void set_ldpc_dec_op(struct rte_bbdev_dec_op **ops,
         ops[j]->ldpc_dec.tb_params.c = 1;
         ops[j]->ldpc_dec.tb_params.r = 0;
         ops[j]->ldpc_dec.tb_params.cab = 1;
-        ops[j]->ldpc_dec.tb_params.ea = nrLDPC_slot_decoding_parameters->TBs[h].segments[i].E;
-        ops[j]->ldpc_dec.tb_params.eb = nrLDPC_slot_decoding_parameters->TBs[h].segments[i].E;
+        ops[j]->ldpc_dec.tb_params.ea = i < p->first_rE2 ? p->E : p->E2;
+        ops[j]->ldpc_dec.tb_params.eb = i < p->first_rE2 ? p->E : p->E2;
       }
       // Calculate offset in the HARQ combined buffers
       // Unique segment offset
-      uint32_t segment_offset = (nrLDPC_slot_decoding_parameters->TBs[h].harq_unique_pid * NR_LDPC_MAX_NUM_CB) + i;
+      uint32_t segment_offset = (p->harq_unique_pid * NR_LDPC_MAX_NUM_CB) + i;
       // Prune to avoid shooting above maximum id
       uint32_t pruned_segment_offset = segment_offset % active_dev.num_harq_codeblock;
       // Segment offset to byte offset
@@ -673,6 +673,7 @@ static void set_ldpc_dec_op(struct rte_bbdev_dec_op **ops,
       ops[j]->ldpc_dec.input = inputs[j];
       ++j;
     }
+    p->d_to_be_cleared = false;
   }
 }
 
@@ -729,14 +730,16 @@ static int retrieve_ldpc_dec_op(struct rte_bbdev_dec_op **ops, nrLDPC_slot_decod
 {
   int j = 0;
   for (int h = 0; h < nrLDPC_slot_decoding_parameters->nb_TBs; ++h) {
-    for (int i = 0; i < nrLDPC_slot_decoding_parameters->TBs[h].C; ++i) {
+    nrLDPC_TB_decoding_parameters_t *p = &nrLDPC_slot_decoding_parameters->TBs[h];
+    size_t data_off = 0;
+    for (int i = 0; i < p->C; ++i) {
       struct rte_bbdev_op_data *hard_output = &ops[j]->ldpc_dec.hard_output;
       struct rte_mbuf *m = hard_output->data;
       uint16_t data_len = rte_pktmbuf_data_len(m) - hard_output->offset;
       uint8_t *data = rte_pktmbuf_mtod_offset(m, uint8_t *, hard_output->offset);
-      memcpy(nrLDPC_slot_decoding_parameters->TBs[h].segments[i].c, data, data_len);
+      memcpy(p->c + data_off, data, data_len);
 
-      uint32_t segment_offset = (nrLDPC_slot_decoding_parameters->TBs[h].harq_unique_pid * NR_LDPC_MAX_NUM_CB) + i;
+      uint32_t segment_offset = (p->harq_unique_pid * NR_LDPC_MAX_NUM_CB) + i;
       uint32_t pruned_segment_offset = segment_offset % active_dev.num_harq_codeblock;
       struct rte_bbdev_op_data *harq_output = &ops[j]->ldpc_dec.harq_combined_output;
       if (!active_dev.support_internal_harq_memory) {
@@ -749,6 +752,7 @@ static int retrieve_ldpc_dec_op(struct rte_bbdev_dec_op **ops, nrLDPC_slot_decod
       active_dev.harq_buffers[pruned_segment_offset].offset = harq_output->offset;
       active_dev.harq_buffers[pruned_segment_offset].length = harq_output->length;
       ++j;
+      data_off += p->K >> 3;
     }
   }
   return 0;
@@ -823,7 +827,7 @@ static int pmd_lcore_ldpc_dec(void *arg)
 
   // Start timer
   // We report timing only once in (0,0) since the timers are merged at the end
-  start_meas(&nrLDPC_slot_decoding_parameters->TBs[0].segments[0].ts_ldpc_decode);
+  start_meas(&nrLDPC_slot_decoding_parameters->TBs[0].ts_ldpc_decode);
 
   uint16_t enq = 0, deq = 0;
   while (enq < num_segments) {
@@ -840,7 +844,7 @@ static int pmd_lcore_ldpc_dec(void *arg)
 
   // Stop timer
   // We report timing only once in (0,0) since the timers are merged at the end
-  stop_meas(&nrLDPC_slot_decoding_parameters->TBs[0].segments[0].ts_ldpc_decode);
+  stop_meas(&nrLDPC_slot_decoding_parameters->TBs[0].ts_ldpc_decode);
 
   if (deq == enq) {
     ret = retrieve_ldpc_dec_op(ops_deq, nrLDPC_slot_decoding_parameters);
@@ -849,24 +853,24 @@ static int pmd_lcore_ldpc_dec(void *arg)
     /* get the max of iter_count for all dequeued ops */
     int j = 0;
     for (int h = 0; h < nrLDPC_slot_decoding_parameters->nb_TBs; ++h) {
-      for (int i = 0; i < nrLDPC_slot_decoding_parameters->TBs[h].C; ++i) {
-        bool *status = &nrLDPC_slot_decoding_parameters->TBs[h].segments[i].decodeSuccess;
+      nrLDPC_TB_decoding_parameters_t *p = &nrLDPC_slot_decoding_parameters->TBs[h];
+      for (int i = 0; i < p->C; ++i) {
+        bool *status = &p->decodeSuccess[i];
         tp->iter_count = RTE_MAX(ops_enq[j]->ldpc_dec.iter_count, tp->iter_count);
 
         // Check if CRC is available otherwise rely on ops_enq[j]->status to detect decoding success
         // CRC is NOT available if the CRC type is 24_B which is when C is greater than 1
-        if (nrLDPC_slot_decoding_parameters->TBs[h].C > 1) {
+        if (p->C > 1) {
           *status = (ops_enq[j]->status == 0);
         } else {
-          uint8_t *decoded_bytes = nrLDPC_slot_decoding_parameters->TBs[h].segments[i].c;
-          uint8_t crc_type = crcType(nrLDPC_slot_decoding_parameters->TBs[h].C, nrLDPC_slot_decoding_parameters->TBs[h].A);
-          uint32_t len_with_crc = lenWithCrc(nrLDPC_slot_decoding_parameters->TBs[h].C, nrLDPC_slot_decoding_parameters->TBs[h].A);
+          uint8_t *decoded_bytes = p->c;
+          uint8_t crc_type = crcType(p->C, p->A);
+          uint32_t len_with_crc = lenWithCrc(p->C, p->A);
           *status = check_crc(decoded_bytes, len_with_crc, crc_type);
         }
 
         if (*status) {
-          *nrLDPC_slot_decoding_parameters->TBs[h].processedSegments =
-              *nrLDPC_slot_decoding_parameters->TBs[h].processedSegments + 1;
+          *p->processedSegments = *p->processedSegments + 1;
         }
         ++j;
       }
@@ -1283,28 +1287,24 @@ int32_t nrLDPC_coding_decoder(nrLDPC_slot_decoding_parameters_t *nrLDPC_slot_dec
   int8_t llr_decimal = (active_dev.info.drv.capabilities)[RTE_BBDEV_OP_LDPC_DEC].cap.ldpc_dec.llr_decimals;
   int offset = 0;
   for (int h = 0; h < nrLDPC_slot_decoding_parameters->nb_TBs; ++h) {
-    for (int r = 0; r < nrLDPC_slot_decoding_parameters->TBs[h].C; r++) {
+    nrLDPC_TB_decoding_parameters_t *p = &nrLDPC_slot_decoding_parameters->TBs[h];
+    size_t data_off = 0;
+    for (int r = 0; r < p->C; r++) {
+      uint32_t E = r < p->first_rE2 ? p->E : p->E2;
       if (active_dev.is_t2) {
         // For the T2, we simply saturate the LLRs.
         uint16_t z_ol[LDPC_MAX_CB_SIZE] __attribute__((aligned(16)));
-        memcpy(z_ol,
-               nrLDPC_slot_decoding_parameters->TBs[h].segments[r].llr,
-               nrLDPC_slot_decoding_parameters->TBs[h].segments[r].E * sizeof(uint16_t));
+        memcpy(z_ol, p->llr + data_off, E * sizeof(uint16_t));
         simde__m128i *pv_ol128 = (simde__m128i *)z_ol;
         simde__m128i *pl_ol128 = (simde__m128i *)&l_ol[offset];
-        for (int i = 0, j = 0; j < ((nrLDPC_slot_decoding_parameters->TBs[h].segments[r].E + 15) >> 4); i += 2, j++) {
+        for (int i = 0, j = 0; j < ((E + 15) >> 4); i += 2, j++) {
           pl_ol128[j] = simde_mm_packs_epi16(pv_ol128[i], pv_ol128[i + 1]);
         }
       } else {
-        llr_scaling(nrLDPC_slot_decoding_parameters->TBs[h].segments[r].llr,
-                    nrLDPC_slot_decoding_parameters->TBs[h].segments[r].E,
-                    &l_ol[offset],
-                    llr_size,
-                    llr_decimal,
-                    nrLDPC_slot_decoding_parameters->TBs[h].nb_layers,
-                    nrLDPC_slot_decoding_parameters->TBs[h].Qm);
+        llr_scaling(p->llr + data_off, E, &l_ol[offset], llr_size, llr_decimal, p->nb_layers, p->Qm);
       }
       offset += LDPC_MAX_CB_SIZE;
+      data_off += E;
     }
   }
 

openair1/PHY/CODING/nrLDPC_coding/nrLDPC_coding_interface.h
@@ -8,42 +8,11 @@
 
 #include "common/utils/time_meas.h"
 #include "openair1/PHY/CODING/coding_defs.h"
+#include "openair1/PHY/CODING/nrLDPC_decoder/nrLDPCdecoder_defs.h"
 #include "common/utils/threadPool/thread-pool.h"
 
 #ifndef __NRLDPC_CODING_INTERFACE__H__
 #define __NRLDPC_CODING_INTERFACE__H__
-
-/**
- * \typedef nrLDPC_segment_decoding_parameters_t
- * \struct nrLDPC_segment_decoding_parameters_s
- * \brief decoding parameter of segments
- * \var E input llr segment size
- * \var R code rate indication
- * \var llr segment input llr array
- * \var d Pointers to code blocks before LDPC decoding (38.212 V15.4.0 section 5.3.2)
- * \var d_to_be_cleared
- * pointer to the flag used to clear d properly
- * when true, clear d after rate dematching
- * \var c Pointers to code blocks after LDPC decoding (38.212 V15.4.0 section 5.2.2)
- * \var decodeSuccess
- * flag indicating that the decoding of the segment was successful
- * IT MUST BE FILLED BY THE IMPLEMENTATION
- * \var ts_deinterleave deinterleaving time stats
- * \var ts_rate_unmatch rate unmatching time stats
- * \var ts_ldpc_decode decoding time stats
- */
-typedef struct nrLDPC_segment_decoding_parameters_s{
-  int E;
-  uint8_t R;
-  short *llr;
-  int16_t *d;
-  bool *d_to_be_cleared;
-  uint8_t *c;
-  bool decodeSuccess;
-  time_stats_t ts_deinterleave;
-  time_stats_t ts_rate_unmatch;
-  time_stats_t ts_ldpc_decode;
-} nrLDPC_segment_decoding_parameters_t;
 
 /**
  * \typedef nrLDPC_TB_decoding_parameters_t
@@ -96,7 +65,20 @@ typedef struct nrLDPC_TB_decoding_parameters_s{
   uint32_t F;
 
   uint32_t C;
-  nrLDPC_segment_decoding_parameters_t *segments;
+  int E;
+  uint8_t R;
+  int E2;
+  uint8_t R2;
+  int first_rE2;
+  short *llr;
+  uint8_t *c;
+  int16_t *d;
+  bool d_to_be_cleared;
+  bool decodeSuccess[NR_LDPC_MAX_NUM_CB];
+  time_stats_t ts_deinterleave;
+  time_stats_t ts_rate_unmatch;
+  time_stats_t ts_seg_prep;
+  time_stats_t ts_ldpc_decode;
 } nrLDPC_TB_decoding_parameters_t;
 
 /**

turkeryilmaz/openairinterface5g

commit 8481bd3eaa676ade0cc37082729a6cf5ffe07f92

Don't require all code blocks to decode in the same HARQ round
Since 28cff29178dd19aa5016571d25fbee2d5f6655dd, a transport block's
payload (harq_process->b) is only assembled from its decoded code blocks
if every single code block reports decodeSuccess in the same HARQ round.
Previously, each code block was copied into b independently, as soon as
its own decodeSuccess was true, regardless of the state of the others
that round.

This gate breaks HARQ combining for T2 that report a failure when
re-decoding a code block that already succeeded in a previous round. The
transport block is retransmitted until it is dropped.

Restore per-segment copying into for current round: a code block that
already succeeded keeps its previously-copied bytes untouched on any
round where it is not currently reporting success.

Fixes: 28cff29178dd ("Simplify TB_parameters, c, d, d_to_be_cleared")
Signed-off-by: Jaroslava Fiedlerova <jaroslava.fiedlerova@openairinterface.org>

openair1/PHY/NR_TRANSPORT/nr_ulsch_decoding.c
@@ -274,28 +274,21 @@ int nr_ulsch_decoding(PHY_VARS_gNB *phy_vars_gNB,
     nrLDPC_TB_decoding_parameters_t *TB_parameters = &TBs[pusch_id];
 
     uint32_t offset = 0, r_offset = 0;
-    bool crcok = true;
     LOG_D(PHY, "C = %d\n", TB_parameters->C);
     for (int r = 0; r < TB_parameters->C; r++) {
       LOG_D(PHY, "Segment %d %d\n", r, TB_parameters->decodeSuccess[r]);
-      if (TB_parameters->decodeSuccess[r] == false) {
+      uint32_t seg_len = (harq_process->K >> 3) - (harq_process->F >> 3) - ((harq_process->C > 1) ? 3 : 0);
+      if (TB_parameters->decodeSuccess[r]) {
+        memcpy(harq_process->b + offset, harq_process->c + r_offset, seg_len);
+      } else {
         LOG_D(PHY, "Segment %d/%d in error\n", r, TB_parameters->C);
-        crcok = false;
-        break;
       }
+      offset += seg_len;
+      r_offset += (harq_process->K >> 3);
     }
-    if (crcok) {
-      for (int r = 0; r < TB_parameters->C; r++) {
-        // Copy c to b in case of decoding success
-        memcpy(harq_process->b + offset,
-               harq_process->c + r_offset,
-               (harq_process->K >> 3) - (harq_process->F >> 3) - ((harq_process->C > 1) ? 3 : 0));
-        offset += ((harq_process->K >> 3) - (harq_process->F >> 3) - ((harq_process->C > 1) ? 3 : 0));
-        r_offset += (harq_process->K >> 3);
-      }
-    } else {
+    bool crcok = (harq_process->processedSegments == TB_parameters->C);
+    if (!crcok)
       LOG_D(PHY, "ULSCH %d in error\n", ULSCH_id);
-    }
     merge_meas(&phy_vars_gNB->ts_deinterleave, &TB_parameters->ts_deinterleave);
     merge_meas(&phy_vars_gNB->ts_rate_unmatch, &TB_parameters->ts_rate_unmatch);
     merge_meas(&phy_vars_gNB->ts_seg_prep, &TB_parameters->ts_seg_prep);