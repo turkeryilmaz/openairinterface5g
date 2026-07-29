merkebu-girmay/oran-72-demo

Add TRP Information Exchange RRC + MAC procedures: Direct
Signed-off-by: Rakesh Mundlamuri <rakesh.mundlamuri@openairinterface.org>

openair2/RRC/NR/rrc_gNB.c
@@ -81,6 +81,9 @@
 #include "alg/find.h"
 #include "NR_HandoverCommand.h"
 #include "openair2/SDAP/nr_sdap/nr_sdap_configuration.h"
+#include "rrc_gNB_NRPPA.h"
+#include "openair2/F1AP/lib/f1ap_positioning.h"
+#include "openair3/NRPPA/nrppa_utils.h"
 
 #ifdef E2_AGENT
 #include "openair2/E2AP/RAN_FUNCTION/O-RAN/ran_func_rc_extern.h"
@@ -3696,6 +3699,16 @@ void *rrc_gnb_task(void *args_p)
       case NGAP_HANDOVER_COMMAND:
         rrc_gNB_process_HandoverCommand(RC.nrrrc[instance], &NGAP_HANDOVER_COMMAND(msg_p));
         rrc_gNB_free_Handover_Command(&NGAP_HANDOVER_COMMAND(msg_p)); // Free transfered NG message
+        break;
+
+      case NRPPA_TRP_INFORMATION_REQ:
+        rrc_gNB_process_trp_information_request(RC.nrrrc[instance], &NRPPA_TRP_INFORMATION_REQ(msg_p));
+        free_nrppa_trp_information_request(&NRPPA_TRP_INFORMATION_REQ(msg_p));
+        break;
+
+      case F1AP_TRP_INFORMATION_RESP:
+        rrc_CU_process_trp_information_response(&F1AP_TRP_INFORMATION_RESP(msg_p));
+        free_trp_information_resp(&F1AP_TRP_INFORMATION_RESP(msg_p));
         break;
 
       default:

turkeryilmaz/openairinterface5g

openair2/RRC/NR/rrc_gNB.c
@@ -80,6 +80,9 @@
 #include "alg/find.h"
 #include "NR_HandoverCommand.h"
 #include "openair2/SDAP/nr_sdap/nr_sdap_configuration.h"
+#include "rrc_gNB_NRPPA.h"
+#include "openair2/F1AP/lib/f1ap_positioning.h"
+#include "openair3/NRPPA/nrppa_gNB_location_information_transfer.h"
 
 #ifdef E2_AGENT
 #include "openair2/E2AP/RAN_FUNCTION/O-RAN/ran_func_rc_extern.h"
@@ -3768,6 +3771,16 @@ void *rrc_gnb_task(void *args_p)
       case NGAP_HANDOVER_COMMAND:
         rrc_gNB_process_HandoverCommand(RC.nrrrc[instance], &NGAP_HANDOVER_COMMAND(msg_p));
         rrc_gNB_free_Handover_Command(&NGAP_HANDOVER_COMMAND(msg_p)); // Free transfered NG message
+        break;
+
+      case NRPPA_TRP_INFORMATION_REQ:
+        rrc_gNB_process_trp_information_request(RC.nrrrc[instance], &NRPPA_TRP_INFORMATION_REQ(msg_p));
+        free_nrppa_trp_information_request(&NRPPA_TRP_INFORMATION_REQ(msg_p));
+        break;
+
+      case F1AP_TRP_INFORMATION_RESP:
+        rrc_CU_process_trp_information_response(&F1AP_TRP_INFORMATION_RESP(msg_p));
+        free_trp_information_resp(&F1AP_TRP_INFORMATION_RESP(msg_p));
         break;
 
       default: