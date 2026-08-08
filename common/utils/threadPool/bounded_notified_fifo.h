turkeryilmaz/openairinterface5g

commit 21dafbcde70fc00b20f89902739c2b75f5513142

New threadpool
 - removed some whitespace noise.
 - cleaned up CMakeLists.txt
 - reintegrated task_manager.c into thread-pool.c
 - cleaned up some unnecessary code

Co-authored-by: Cedric Roux <cedric.roux@eurecom.fr>
Co-authored-by: Mikel Irazabal <mikel.irazabal@openairinterface.com>

common/utils/threadPool/bounded_notified_fifo.h
@@ -1,0 +1,245 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#ifndef BOUNDED_NOTIFIED_FIFO_H
+#define BOUNDED_NOTIFIED_FIFO_H
+
+#include "assertions.h"
+#include <stdint.h>
+#include <memory.h>
+#include "task.h"
+#include <pthread.h>
+#include "pthread_utils.h"
+
+// For working correctly, maintain the default elements to a 2^N e.g., 2^5=32
+#define DEFAULT_ELM 256
+
+typedef struct seq_ring_buf_s {
+  task_t* array;
+  size_t cap;
+  uint32_t head;
+  uint32_t tail;
+  _Atomic uint64_t sz;
+} seq_ring_task_t;
+
+
+static size_t size_seq_ring_task(seq_ring_task_t* r)
+{
+  DevAssert(r != NULL);
+
+  return r->head - r->tail;
+}
+
+static uint32_t mask(uint32_t cap, uint32_t val)
+{
+  return val & (cap - 1);
+}
+
+static bool full(seq_ring_task_t* r)
+{
+  return size_seq_ring_task(r) == r->cap - 1;
+}
+
+static void enlarge_buffer(seq_ring_task_t* r)
+{
+  DevAssert(r != NULL);
+  DevAssert(full(r));
+
+  const uint32_t factor = 2;
+  task_t* tmp_buffer = calloc(r->cap * factor, sizeof(task_t));
+  DevAssert(tmp_buffer != NULL);
+
+  const uint32_t head_pos = mask(r->cap, r->head);
+  const uint32_t tail_pos = mask(r->cap, r->tail);
+
+  if (head_pos > tail_pos) {
+    memcpy(tmp_buffer, r->array + tail_pos, (head_pos - tail_pos) * sizeof(task_t));
+  } else {
+    memcpy(tmp_buffer, r->array + tail_pos, (r->cap - tail_pos) * sizeof(task_t));
+    memcpy(tmp_buffer + (r->cap - tail_pos), r->array, head_pos * sizeof(task_t));
+  }
+  r->cap *= factor;
+  free(r->array);
+  r->array = tmp_buffer;
+  r->tail = 0;
+  r->head = r->cap / 2 - 1;
+}
+
+static void init_seq_ring_task(seq_ring_task_t* r)
+{
+  DevAssert(r != NULL);
+  task_t* tmp_buffer = calloc(DEFAULT_ELM, sizeof(task_t));
+  DevAssert(tmp_buffer != NULL);
+  seq_ring_task_t tmp = {.array = tmp_buffer, .head = 0, .tail = 0, .cap = DEFAULT_ELM};
+  memcpy(r, &tmp, sizeof(seq_ring_task_t));
+  r->sz = 0;
+}
+
+static void free_seq_ring_task(seq_ring_task_t* r)
+{
+  DevAssert(r != NULL);
+  free(r->array);
+}
+
+static void push_back_seq_ring_task(seq_ring_task_t* r, task_t t)
+{
+  DevAssert(r != NULL);
+
+  if (full(r))
+    enlarge_buffer(r);
+
+  const uint32_t pos = mask(r->cap, r->head);
+  r->array[pos] = t;
+  r->head += 1;
+  r->sz += 1;
+}
+
+static task_t pop_seq_ring_task(seq_ring_task_t* r)
+{
+  DevAssert(r != NULL);
+  DevAssert(size_seq_ring_task(r) > 0);
+
+  const uint32_t pos = mask(r->cap, r->tail);
+  task_t t = r->array[pos];
+  r->tail += 1;
+  r->sz -= 1;
+  return t;
+}
+
+#undef DEFAULT_ELM
+
+typedef struct {
+  pthread_mutex_t mtx;
+  pthread_cond_t cv;
+  seq_ring_task_t r;
+  size_t idx;
+} not_q_t;
+
+typedef struct {
+  task_t t;
+  bool success;
+} ret_try_t;
+
+static void init_not_q(not_q_t* q, size_t idx)
+{
+  DevAssert(q != NULL);
+
+  q->idx = idx;
+
+  init_seq_ring_task(&q->r);
+
+  mutexinit(q->mtx);
+  condinit(q->cv);
+}
+
+static void free_not_q(not_q_t* q)
+{
+  DevAssert(q != NULL);
+
+  free_seq_ring_task(&q->r);
+
+  mutexdestroy(q->mtx);
+  conddestroy(q->cv);
+}
+
+static bool try_push_not_q(not_q_t* q, task_t t)
+{
+  DevAssert(q != NULL);
+
+
+  if (mutextrylock(q->mtx) != 0)
+    return false;
+
+  push_back_seq_ring_task(&q->r, t);
+
+  const size_t sz = size_seq_ring_task(&q->r);
+  DevAssert(sz > 0);
+
+  mutexunlock(q->mtx);
+
+  condsignal(q->cv);
+
+  return true;
+}
+
+static void push_not_q(not_q_t* q, task_t t)
+{
+  DevAssert(q != NULL);
+  DevAssert(t.func != NULL);
+
+  mutexlock(q->mtx);
+
+  push_back_seq_ring_task(&q->r, t);
+
+  DevAssert(size_seq_ring_task(&q->r) > 0);
+
+  mutexunlock(q->mtx);
+
+  condsignal(q->cv);
+}
+
+static ret_try_t try_pop_not_q(not_q_t* q)
+{
+  DevAssert(q != NULL);
+
+  ret_try_t ret = {.success = false};
+
+  int rc = mutextrylock(q->mtx);
+  DevAssert(rc == 0 || rc == EBUSY);
+
+  if (rc == EBUSY)
+    return ret;
+
+  size_t sz = size_seq_ring_task(&q->r);
+  if (sz == 0) {
+    mutexunlock(q->mtx);
+
+    return ret;
+  }
+
+  DevAssert(sz > 0);
+  ret.t = pop_seq_ring_task(&q->r);
+
+  mutexunlock(q->mtx);
+  ret.success = true;
+
+  return ret;
+}
+
+static bool pop_not_q(not_q_t* q, ret_try_t* out)
+{
+  DevAssert(q != NULL);
+  DevAssert(out != NULL);
+
+  mutexlock(q->mtx);
+
+  while (size_seq_ring_task(&q->r) == 0) {
+    condwait(q->cv, q->mtx);
+  }
+
+  out->t = pop_seq_ring_task(&q->r);
+
+  mutexunlock(q->mtx);
+
+  return true;
+}
+
+#endif

common/utils/telnetsrv/telnetsrv.c
@@ -53,7 +53,6 @@
 #include <sys/resource.h>
 #include "common/utils/load_module_shlib.h"
 #include "common/config/config_userapi.h"
-#include "common/utils/threadPool/thread-pool.h"
 #include "executables/softmodem-common.h"
 #include <readline/history.h>
 #include "common/oai_version.h"

common/utils/threadPool/CMakeLists.txt
@@ -1,6 +1,6 @@
 if (ENABLE_TESTS)
   add_subdirectory(test)
 endif()
-add_library(thread-pool thread-pool.c)
+add_library(thread-pool thread-pool.c task_ans.c)
 target_link_libraries(thread-pool PRIVATE utils)
 target_include_directories(thread-pool PUBLIC .)

common/utils/threadPool/notified_fifo.h
@@ -1,0 +1,223 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#ifndef NOTIFIED_FIFO_H
+#define NOTIFIED_FIFO_H
+#include "pthread_utils.h"
+#include <stdint.h>
+#include <pthread.h>
+#include "time_meas.h"
+#include <memory.h>
+#include <stdalign.h>
+#include "assertions.h"
+
+/// @brief Element on the notifiedFifo_t
+/// next: internal FIFO chain, do not set it
+/// key: a long int that the client can use to identify a job or a group of messages
+/// ResponseFifo: if the client defines a response FIFO, the job will be posted back after processing
+/// processingFunc: any function (of type void processingFunc(void *)) that a worker will process
+/// msgData: the data passed to `processingFunc`. It can be added automatically, or you can set it to a buffer you are managing
+/// malloced: a boolean that enables internal free in the case of no return FIFO or abort feature
+typedef struct notifiedFIFO_elt_s {
+  struct notifiedFIFO_elt_s *next;
+  uint64_t key; // To filter out elements
+  struct notifiedFIFO_s *reponseFifo;
+  void (*processingFunc)(void *);
+  bool malloced;
+  oai_cputime_t creationTime;
+  oai_cputime_t startProcessingTime;
+  oai_cputime_t endProcessingTime;
+  oai_cputime_t returnTime;
+  // use alignas(32) to align msgData to 32b
+  // user data behind it will be aligned to 32b as well
+  // important! this needs to be the last member in the struct
+  alignas(32) void *msgData;
+} notifiedFIFO_elt_t;
+
+typedef struct notifiedFIFO_s {
+  notifiedFIFO_elt_t *outF;
+  notifiedFIFO_elt_t *inF;
+  pthread_mutex_t lockF;
+  pthread_cond_t notifF;
+  bool abortFIFO; // if set, the FIFO always returns NULL -> abort condition
+} notifiedFIFO_t;
+
+/// @brief Creates a new job.
+/// @param size The data part of the job will have this size
+/// @param key the job can be identified
+/// @param reponseFifo response fifo
+/// @param processingFunc function to call
+/// @return new notifiedFIFO_elt_t element with extra memory allocated at the end equal to size.
+static inline notifiedFIFO_elt_t *newNotifiedFIFO_elt(int size,
+                                                      uint64_t key,
+                                                      notifiedFIFO_t *reponseFifo,
+                                                      void (*processingFunc)(void *))
+{
+  notifiedFIFO_elt_t *ret = (notifiedFIFO_elt_t *)memalign(32, sizeof(notifiedFIFO_elt_t) + size);
+  AssertFatal(NULL != ret, "out of memory\n");
+  ret->next = NULL;
+  ret->key = key;
+  ret->reponseFifo = reponseFifo;
+  ret->processingFunc = processingFunc;
+  // We set user data piece aligend 32 bytes to be able to process it with SIMD
+  // msgData is aligned to 32bytes, so everything after will be as well
+  ret->msgData = ((uint8_t *)ret) + sizeof(notifiedFIFO_elt_t);
+  ret->malloced = true;
+  return ret;
+}
+
+/// @brief Get pointer to the data carried by notifiedFIFO_elt_t
+/// @param elt
+/// @return void pointer to the data allocated with the message
+static inline void *NotifiedFifoData(notifiedFIFO_elt_t *elt)
+{
+  return elt->msgData;
+}
+
+/// @brief Delete a notifiedFIFO_elt_t if its allocated
+/// @param elt
+static inline void delNotifiedFIFO_elt(notifiedFIFO_elt_t *elt)
+{
+  if (elt->malloced) {
+    elt->malloced = false;
+    free(elt);
+  }
+}
+
+static inline void initNotifiedFIFO_nothreadSafe(notifiedFIFO_t *nf)
+{
+  nf->inF = NULL;
+  nf->outF = NULL;
+  nf->abortFIFO = false;
+}
+static inline void initNotifiedFIFO(notifiedFIFO_t *nf)
+{
+  mutexinit(nf->lockF);
+  condinit(nf->notifF);
+  initNotifiedFIFO_nothreadSafe(nf);
+  // No delete function: the creator has only to free the memory
+}
+
+static inline void pushNotifiedFIFO_nothreadSafe(notifiedFIFO_t *nf, notifiedFIFO_elt_t *msg)
+{
+  msg->next = NULL;
+
+  if (nf->outF == NULL)
+    nf->outF = msg;
+
+  if (nf->inF != NULL)
+    nf->inF->next = msg;
+
+  nf->inF = msg;
+}
+
+static inline void pushNotifiedFIFO(notifiedFIFO_t *nf, notifiedFIFO_elt_t *msg)
+{
+  mutexlock(nf->lockF);
+  if (!nf->abortFIFO) {
+    pushNotifiedFIFO_nothreadSafe(nf, msg);
+    condsignal(nf->notifF);
+  }
+  mutexunlock(nf->lockF);
+}
+
+static inline notifiedFIFO_elt_t *pullNotifiedFIFO_nothreadSafe(notifiedFIFO_t *nf)
+{
+  if (nf->outF == NULL)
+    return NULL;
+  if (nf->abortFIFO)
+    return NULL;
+
+  notifiedFIFO_elt_t *ret = nf->outF;
+
+  AssertFatal(nf->outF != nf->outF->next, "Circular list in thread pool: push several times the same buffer is forbidden\n");
+
+  nf->outF = nf->outF->next;
+
+  if (nf->outF == NULL)
+    nf->inF = NULL;
+
+  return ret;
+}
+
+static inline notifiedFIFO_elt_t *pullNotifiedFIFO(notifiedFIFO_t *nf)
+{
+  mutexlock(nf->lockF);
+  notifiedFIFO_elt_t *ret = NULL;
+
+  while ((ret = pullNotifiedFIFO_nothreadSafe(nf)) == NULL && !nf->abortFIFO)
+    condwait(nf->notifF, nf->lockF);
+
+  mutexunlock(nf->lockF);
+  return ret;
+}
+
+static inline notifiedFIFO_elt_t *pollNotifiedFIFO(notifiedFIFO_t *nf)
+{
+  int tmp = mutextrylock(nf->lockF);
+
+  if (tmp != 0)
+    return NULL;
+
+  if (nf->abortFIFO) {
+    mutexunlock(nf->lockF);
+    return NULL;
+  }
+
+  notifiedFIFO_elt_t *ret = pullNotifiedFIFO_nothreadSafe(nf);
+  mutexunlock(nf->lockF);
+  return ret;
+}
+
+static inline time_stats_t exec_time_stats_NotifiedFIFO(const notifiedFIFO_elt_t *elt)
+{
+  time_stats_t ts = {0};
+  if (elt->startProcessingTime == 0 && elt->endProcessingTime == 0)
+    return ts; /* no measurements done */
+  ts.in = elt->startProcessingTime;
+  ts.diff = elt->endProcessingTime - ts.in;
+  ts.p_time = ts.diff;
+  ts.diff_square = ts.diff * ts.diff;
+  ts.max = ts.diff;
+  ts.trials = 1;
+  return ts;
+}
+
+// This functions aborts all messages in the queue, and marks the queue as
+// "aborted", such that every call to it will return NULL
+static inline void abortNotifiedFIFO(notifiedFIFO_t *nf)
+{
+  mutexlock(nf->lockF);
+  nf->abortFIFO = true;
+  notifiedFIFO_elt_t **elt = &nf->outF;
+  while (*elt != NULL) {
+    notifiedFIFO_elt_t *p = *elt;
+    *elt = (*elt)->next;
+    delNotifiedFIFO_elt(p);
+  }
+
+  if (nf->outF == NULL)
+    nf->inF = NULL;
+  condbroadcast(nf->notifF);
+  mutexunlock(nf->lockF);
+}
+
+#endif

common/utils/threadPool/pthread_utils.h
@@ -1,0 +1,45 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#ifndef PTHREAD_UTILS_H
+#define PTHREAD_UTILS_H
+
+#include <pthread.h>
+#define mutexinit(mutex)   {int ret=pthread_mutex_init(&mutex,NULL); \
+                            AssertFatal(ret==0,"ret=%d\n",ret);}
+#define condinit(signal)   {int ret=pthread_cond_init(&signal,NULL); \
+                            AssertFatal(ret==0,"ret=%d\n",ret);}
+#define mutexlock(mutex)   {int ret=pthread_mutex_lock(&mutex); \
+                            AssertFatal(ret==0,"ret=%d\n",ret);}
+#define mutextrylock(mutex)   pthread_mutex_trylock(&mutex)
+#define mutexunlock(mutex) {int ret=pthread_mutex_unlock(&mutex); \
+                            AssertFatal(ret==0,"ret=%d\n",ret);}
+#define condwait(condition, mutex) {int ret=pthread_cond_wait(&condition, &mutex); \
+                                    AssertFatal(ret==0,"ret=%d\n",ret);}
+#define condbroadcast(signal) {int ret=pthread_cond_broadcast(&signal); \
+                               AssertFatal(ret==0,"ret=%d\n",ret);}
+#define condsignal(signal)    {int ret=pthread_cond_signal(&signal); \
+                               AssertFatal(ret==0,"ret=%d\n",ret);}
+#define mutexdestroy(mutex) { int ret = pthread_mutex_destroy(&mutex);\
+                              AssertFatal(ret==0,"ret=%d\n",ret);}
+#define conddestroy(condition) { int ret = pthread_cond_destroy(&condition);\
+                                 AssertFatal(ret==0,"ret=%d\n",ret);}
+#endif

common/utils/threadPool/task.h
@@ -19,34 +19,12 @@
  *      contact@openairinterface.org
  */
 
-#ifndef ACTOR_H
-#define ACTOR_H
-#include "notified_fifo.h"
+#ifndef TASK_WORK_STEALING_THREAD_POOL_H
+#define TASK_WORK_STEALING_THREAD_POOL_H
 
-#define INIT_ACTOR(ptr, name, core_affinity) init_actor((Actor_t *)ptr, name, core_affinity);
-
-#define DESTROY_ACTOR(ptr) destroy_actor((Actor_t *)ptr);
-
-#define SHUTDOWN_ACTOR(ptr) shutdown_actor((Actor_t *)ptr);
-
-typedef struct Actor_t {
-  notifiedFIFO_t fifo;
-  bool terminate;
-  pthread_t thread;
-} Actor_t;
-
-/// @brief Initialize the actor. Starts actor thread
-/// @param actor
-/// @param name Actor name. Thread name will be derived from it
-/// @param core_affinity Core affinity. Specify -1 for no affinity
-void init_actor(Actor_t *actor, const char *name, int core_affinity);
-
-/// @brief Destroy the actor. Free the memory, stop the thread.
-/// @param actor
-void destroy_actor(Actor_t *actor);
-
-/// @brief Gracefully shutdown the actor, letting all tasks previously started finish
-/// @param actor
-void shutdown_actor(Actor_t *actor);
+typedef struct {
+  void* args;
+  void (*func)(void* args);
+} task_t;
 
 #endif

common/utils/threadPool/task_ans.c
@@ -19,34 +19,44 @@
  *      contact@openairinterface.org
  */
 
-#ifndef ACTOR_H
-#define ACTOR_H
-#include "notified_fifo.h"
+#include "task_ans.h"
+#include "assertions.h"
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define INIT_ACTOR(ptr, name, core_affinity) init_actor((Actor_t *)ptr, name, core_affinity);
+void completed_task_ans(task_ans_t* task)
+{
+  DevAssert(task != NULL);
 
-#define DESTROY_ACTOR(ptr) destroy_actor((Actor_t *)ptr);
+  if (atomic_load_explicit(&task->status, memory_order_acquire) != 0)
+    AssertFatal(0, "Task already finished?");
 
-#define SHUTDOWN_ACTOR(ptr) shutdown_actor((Actor_t *)ptr);
+  atomic_store_explicit(&task->status, 1, memory_order_release);
+}
 
-typedef struct Actor_t {
-  notifiedFIFO_t fifo;
-  bool terminate;
-  pthread_t thread;
-} Actor_t;
+void join_task_ans(task_ans_t* arr, size_t len)
+{
+  DevAssert(len < INT_MAX);
+  DevAssert(arr != NULL);
 
-/// @brief Initialize the actor. Starts actor thread
-/// @param actor
-/// @param name Actor name. Thread name will be derived from it
-/// @param core_affinity Core affinity. Specify -1 for no affinity
-void init_actor(Actor_t *actor, const char *name, int core_affinity);
-
-/// @brief Destroy the actor. Free the memory, stop the thread.
-/// @param actor
-void destroy_actor(Actor_t *actor);
-
-/// @brief Gracefully shutdown the actor, letting all tasks previously started finish
-/// @param actor
-void shutdown_actor(Actor_t *actor);
-
-#endif
+  // Spin lock inspired by:
+  // The Art of Writing Efficient Programs:
+  // An advanced programmer's guide to efficient hardware utilization
+  // and compiler optimizations using C++ examples
+  const struct timespec ns = {0, 1};
+  uint64_t i = 0;
+  int j = len - 1;
+  for (; j != -1; i++) {
+    for (; j != -1; --j) {
+      int const task_completed = 1;
+      if (atomic_load_explicit(&arr[j].status, memory_order_acquire) != task_completed)
+        break;
+    }
+    if (i % 8 == 0) {
+      nanosleep(&ns, NULL);
+    }
+  }
+}

common/utils/threadPool/task_ans.h
@@ -1,0 +1,74 @@
+/*
+ * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The OpenAirInterface Software Alliance licenses this file to You under
+ * the OAI Public License, Version 1.1  (the "License"); you may not use this file
+ * except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.openairinterface.org/?page_id=698
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *-------------------------------------------------------------------------------
+ * For more information about the OpenAirInterface (OAI) Software Alliance:
+ *      contact@openairinterface.org
+ */
+
+#ifndef TASK_ANSWER_THREAD_POOL_H
+#define TASK_ANSWER_THREAD_POOL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef __cplusplus
+#include <stdalign.h>
+#include <stdatomic.h>
+#else
+#include <atomic>
+#define _Atomic(X) std::atomic<X>
+#define _Alignas(X) alignas(X)
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+
+#if defined(__i386__) || defined(__x86_64__)
+#define LEVEL1_DCACHE_LINESIZE 64
+#elif defined(__aarch64__)
+// This is not always true for ARM
+// in linux, you can obtain the size at runtime using sysconf (_SC_LEVEL1_DCACHE_LINESIZE)
+// or from the bash with the command $ getconf LEVEL1_DCACHE_LINESIZE
+// in c++ using std::hardware_destructive_interference_size
+#define LEVEL1_DCACHE_LINESIZE 64
+#else
+#error Unknown CPU architecture
+#endif
+
+typedef struct {
+  // Avoid false sharing
+  _Alignas(LEVEL1_DCACHE_LINESIZE) _Atomic(int) status;
+} task_ans_t;
+
+typedef struct {
+  uint8_t* buf;
+  size_t len;
+  size_t cap; // capacity
+  task_ans_t* ans;
+} thread_info_tm_t;
+
+
+void join_task_ans(task_ans_t* arr, size_t len);
+
+void completed_task_ans(task_ans_t* task);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

commit 6e842c67408221adfd564fefb99d32085b68c59d

fix(thread-pool): validate tasks at queue insertion
abortTpool() and worker_thread() use an all-null task as the shutdown
sentinel. Queue submission first tries nonblocking locks and then falls back
to a blocking push, but only the blocking path rejected that sentinel.
Shutdown could therefore assert when contention selected the fallback.

Move the validity check to the sole ring-buffer insertion point. Callable
tasks remain valid with any argument, the exact all-null sentinel is accepted,
and a null-function task carrying arguments is rejected before enqueue.

A local-only six-case regression moved from 4/6 expected outcomes before the
change to 6/6 after it. test_thread-pool passed 20/20 in Debug and 5/5 with
ASan/UBSan and leak detection; nr-uesoftmodem rebuilt and linked successfully.

Fixes: 21dafbcde7 ("New threadpool")
Assisted-by: ChatGPT:gpt-5.6-sol
Signed-off-by: Turker Yilmaz <turker87@gmail.com>

common/utils/threadPool/bounded_notified_fifo.h
@@ -85,6 +85,7 @@ static void free_seq_ring_task(seq_ring_task_t* r)
 static void push_back_seq_ring_task(seq_ring_task_t* r, task_t t)
 {
   DevAssert(r != NULL);
+  DevAssert(t.func != NULL || t.args == NULL);
 
   if (full(r))
     enlarge_buffer(r);
@@ -166,7 +167,6 @@ static bool try_push_not_q(not_q_t* q, task_t t)
 static void push_not_q(not_q_t* q, task_t t)
 {
   DevAssert(q != NULL);
-  DevAssert(t.func != NULL);
 
   mutexlock(q->mtx);
 