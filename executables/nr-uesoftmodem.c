turkeryilmaz/openairinterface5g

commit 8601b8c99026bfe48dcedab6395faeacd0ba4d64

Fix IQ recorder/player for nrUE

executables/nr-uesoftmodem.c
@@ -211,7 +211,8 @@ int create_tasks_nrue(uint32_t ue_nb) {
   return 0;
 }
 
-void exit_function(const char *file, const char *function, const int line, const char *s) {
+void exit_function(const char *file, const char *function, const int line, const char *s, const int assert)
+{
   int CC_id;
 
   if (s != NULL) {
@@ -227,8 +228,12 @@ void exit_function(const char *file, const char *function, const int line, const
     }
   }
 
-  sleep(1); //allow lte-softmodem threads to exit first
-  exit(1);
+  if (assert) {
+    abort();
+  } else {
+    sleep(1); // allow lte-softmodem threads to exit first
+    exit(EXIT_SUCCESS);
+  }
 }
 
 uint64_t get_nrUE_optmask(void) {

cmake_targets/build_oai
@@ -55,7 +55,7 @@ CMAKE_BUILD_TYPE="RelWithDebInfo"
 CMAKE_CMD="$CMAKE"
 BUILD_ECLIPSE=0
 NR="False"
-OPTIONAL_LIBRARIES="telnetsrv enbscope uescope nrscope"
+OPTIONAL_LIBRARIES="telnetsrv enbscope uescope nrscope oai_iqplayer"
 ulfiusdep=$(basename ./`find /usr/lib* -name libulfius.so`)
 jssondep=$(basename ./`find /usr/lib* -name libjansson.so`)
 if [ "$ulfiusdep" == "libulfius.so" -a "$jssondep" == "libjansson.so" ] ; then

common/utils/T/check_vcd.c
@@ -19,7 +19,19 @@
 #define ENABLE_USE_CPU_EXECUTION_TIME
 #include "../LOG/vcd_signal_dumper.c"
 
-void err(char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
+/*
+ * Dummy needed by assertions.h
+ */
+void exit_function(const char *file, const char *function, const int line, const char *s, const int assert)
+{
+  if (assert) {
+    abort();
+  } else {
+    exit(EXIT_SUCCESS);
+  }
+}
+
+void err(char *fmt, ...) __attribute__((format(printf, 1, 2)));
 void err(char *fmt, ...)
 {
   va_list ap;

common/utils/assertions.h
@@ -30,16 +30,19 @@
 #include <platform_types.h>
 #include "backtrace.h"
 
-#define _Assert_Exit_							\
-  if (getenv("gdbStacks")) {						\
-    char tmp [1000];							\
-    sprintf(tmp,"gdb -ex='set confirm off' -ex 'thread apply all bt' -ex q -p %d < /dev/null", getpid());  \
-    __attribute__((unused)) int dummy=system(tmp);						\
-  }									\
-  fprintf(stderr, "\nExiting execution\n");				\
-  fflush(stdout);							\
-  fflush(stderr);							\
-  abort();
+#define EXIT_NORMAL 0
+#define EXIT_ASSERT 1
+
+#define _Assert_Exit_                                                                                      \
+  if (getenv("gdbStacks")) {                                                                               \
+    char tmp[1000];                                                                                        \
+    sprintf(tmp, "gdb -ex='set confirm off' -ex 'thread apply all bt' -ex q -p %d < /dev/null", getpid()); \
+    __attribute__((unused)) int dummy = system(tmp);                                                       \
+  }                                                                                                        \
+  fprintf(stderr, "\nExiting execution\n");                                                                \
+  fflush(stdout);                                                                                          \
+  fflush(stderr);                                                                                          \
+  exit_function(__FILE__, __FUNCTION__, __LINE__, "_Assert_Exit_", EXIT_ASSERT);
 
 #define _Assert_(cOND, aCTION, fORMAT, aRGS...)             \
 do {                                                        \

common/utils/minimal_stub.c
@@ -2,5 +2,11 @@
 int T_stdout;
 #endif
 
-void exit_function(const char *file, const char *function, const int line, const char *s) {
+void exit_function(const char *file, const char *function, const int line, const char *s, const int assert)
+{
+  if (assert) {
+    abort();
+  } else {
+    exit(EXIT_SUCCESS);
+  }
 }

common/utils/threadPool/thread-pool.c
@@ -176,7 +176,13 @@ void initFloatingCoresTpool(int nbThreads,tpool_t *pool, bool performanceMeas, c
 #ifdef TEST_THREAD_POOL
 volatile int oai_exit=0;
 
-void exit_function(const char *file, const char *function, const int line, const char *s) {
+void exit_function(const char *file, const char *function, const int line, const char *s, const int assert)
+{
+  if (assert) {
+    abort();
+  } else {
+    exit(EXIT_SUCCESS);
+  }
 }
 
 struct testData {

executables/lte-softmodem.c
@@ -236,8 +236,8 @@ unsigned int build_rfdc(int dcoff_i_rxfe, int dcoff_q_rxfe) {
   return (dcoff_i_rxfe + (dcoff_q_rxfe<<8));
 }
 
-
-void exit_function(const char *file, const char *function, const int line, const char *s) {
+void exit_function(const char *file, const char *function, const int line, const char *s, const int assert)
+{
   int ru_id;
 
   if (s != NULL) {
@@ -261,8 +261,12 @@ void exit_function(const char *file, const char *function, const int line, const
     }
   }
 
-  sleep(1); //allow lte-softmodem threads to exit first
-  exit(1);
+  if (assert) {
+    abort();
+  } else {
+    sleep(1); // allow lte-softmodem threads to exit first
+    exit(EXIT_SUCCESS);
+  }
 }
 
 

executables/lte-uesoftmodem.c
@@ -244,7 +244,7 @@ unsigned int build_rfdc(int dcoff_i_rxfe, int dcoff_q_rxfe) {
 }
 
 
-void exit_function(const char *file, const char *function, const int line, const char *s) {
+void exit_function(const char *file, const char *function, const int line, const char *s, const int assert) {
   int CC_id;
   logClean();
   printf("%s:%d %s() Exiting OAI softmodem: %s\n",file,line, function, ((s==NULL)?"":s));
@@ -258,12 +258,15 @@ void exit_function(const char *file, const char *function, const int line, const
             PHY_vars_UE_g[0][CC_id]->rfdevice.trx_end_func(&PHY_vars_UE_g[0][CC_id]->rfdevice);
   }
 
-  sleep(1); //allow lte-softmodem threads to exit first
-
   if(PHY_vars_UE_g != NULL )
     itti_terminate_tasks (TASK_UNKNOWN);
 
-  exit(1);
+  if (assert) {
+    abort();
+  } else {
+    sleep(1); // allow lte-softmodem threads to exit first
+    exit(EXIT_SUCCESS);
+  }
 }
 
 extern int16_t dlsch_demod_shift;

commit 0e6cebb63a98963211a3c577db73c78f2f5f8c50

fix(nr-ue): avoid radio teardown on fatal exit
AssertFatal may run on a PHY worker while peer actors still own radio
work. Ending the radio from that worker can mask the primary failure
with a secondary backend abort.

Abort assertions before touching shared shutdown or radio state. Keep
the existing stop, end, delay, and successful exit for non-assert
requests.

Fixes: 8601b8c99026 ("Fix IQ recorder/player for nrUE")
Assisted-by: ChatGPT:gpt-5.6-sol
Signed-off-by: Turker Yilmaz <turker87@gmail.com>

executables/nr-uesoftmodem.c
@@ -106,14 +106,13 @@ void exit_function(const char *file, const char *function, const int line, const
 {
   LOG_W(NR_PHY, "called by: %s:%d %s() Exiting OAI softmodem: %s\n", file, line, function, s ? s : "no msg");
 
-  oai_exit = 1;
-
-  nrue_ru_stop();
-  nrue_ru_end();
-
   if (assert) {
     abort();
   } else {
+    oai_exit = 1;
+    nrue_ru_stop();
+    nrue_ru_end();
+
     sleep(1); // allow lte-softmodem threads to exit first
     exit(EXIT_SUCCESS);
   }