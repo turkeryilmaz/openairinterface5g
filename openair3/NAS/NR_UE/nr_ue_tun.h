910b90d3fc6e2b91773d6412f5c2ea31bb0c2c62
Guido Casati <guido.casati@openairinterface.org>
2026-08-27 00:17:41 +0200

    nas: add nr_ue_tun module for PDUSession-scoped TUN state
    PDU session IP, QFI, and the OS TUN fd must live with NAS, not SDAP:
    at 5GMM-IDLE the SDAP entity is gone but the 5GSM session remains, and
    MO Service Request (TS 24.501 clause 5.6.1.1 d) needs that state for
    Uplink data status and to re-bind user plane after UP restore.
    
    Add nr_ue_tun helpers that create TUN/TAP into nas_ue_pdu_tun_t and
    store QFI, plus a reader_thread field for later start/stop. Link the
    new static lib into nr_nas.
    
    In a later commit NAS PDU accept will call nr_ue_tun_* instead
    of SDAP create_ue_*, sdap_tun_iface_list will be dropped, and
    reader will be started after SR on the info stored in pdu_tun
    via nr_sdap_tun_start_reader.
    
    Changes:
    - nr_ue_tun.c/h: nr_ue_tun_create_ip_if, create_eth_if, store_qfi
    - nr_ue_tun.h: add reader_thread on nas_ue_pdu_tun_t
    - CMakeLists.txt: nr_ue_tun static lib, nr_nas links nr_ue_tun
    
    Signed-off-by: Guido Casati <guido.casati@openairinterface.org>

--- openair3/NAS/NR_UE/nr_ue_tun.h
+++ openair3/NAS/NR_UE/nr_ue_tun.h
@@ -5,12 +5,21 @@
 #ifndef NR_UE_TUN_H
 #define NR_UE_TUN_H
 
+#include <pthread.h>
+#include <stdint.h>
 #include <net/if.h>
 
 typedef struct nas_ue_pdu_tun_s {
   int sock; /* primary TUN fd (-1 = none) */
   char ifname[IFNAMSIZ];
   int qfi; /* -1 = unset */
+  /** TUN reader (idle or connected)
+   * lifecycle managed by SDAP start/stop APIs */
+  pthread_t reader_thread;
 } nas_ue_pdu_tun_t;
+
+void nr_ue_tun_create_ip_if(nas_ue_pdu_tun_t *t, const char *ipv4, const char *ipv6, int ue_id, int pdu_session_id);
+void nr_ue_tun_create_eth_if(nas_ue_pdu_tun_t *t, int ue_id, int pdu_session_id);
+void nr_ue_tun_store_qfi(nas_ue_pdu_tun_t *t, uint8_t qfi);
 
 #endif /* NR_UE_TUN_H */
